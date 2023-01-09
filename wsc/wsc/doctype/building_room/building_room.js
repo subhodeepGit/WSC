74e794035be9e6c7c7e6301be04b4b1a56c6e103
shivkaul01 <shiv.kaul@soulunileaders.com>
2023-01-09 16:31:09 +0530

    Building room modify

--- wsc/wsc/doctype/building_room/building_room.js
+++ wsc/wsc/doctype/building_room/building_room.js
@@ -2,3 +2,15 @@
 // For license information, please see license.txt
 
 
+frappe.ui.form.on("Building Room", {
+	setup: function(frm) {
+		frm.set_query("residence_type_name", function() {
+			return {
+				filters: [
+					["Quarter Type","type_of_residence", "in", [frm.doc.type_of_residence]],
+                    
+				]
+			}
+		});
+	}
+});
