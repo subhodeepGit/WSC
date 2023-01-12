ead8b5754be3ce994b95a0b0154df38df9720371
Subhajit-SOUL <subhajit.saha@soulunileaders.com>
2023-01-12 19:23:05 +0530

    Updated Code

--- wsc/wsc/validations/instructor.py
+++ wsc/wsc/validations/instructor.py
@@ -4,23 +4,18 @@ from wsc.wsc.doctype.user_permission import add_user_permission,delete_ref_docty
 from wsc.wsc.utils import semester_belongs_to_programs,academic_term,duplicate_row_validation,get_courses_by_semester
 
 def validate(doc,method):
-    print("\n\n\n\n\n")
-    print("ok")
     update_user(doc)
     # permission(doc)
     # director_permission(doc)
     validate_instructor_log(doc)
     academic_term(doc)
 
-    for t in doc.get("other_activities"):
-        print(t.duration)
-
     class_scheduled = frappe.db.sql("""Select count(*) from `tabCourse Schedule` where instructor = %s""",doc.name)
     doc.total_scheduled_classes= class_scheduled[0][0]
     class_taken = frappe.db.sql("""Select count(*) from `tabStudent Attendance` where instructor = %s""",doc.name)
     doc.total_classes_taken = class_taken[0][0]
 
-    work_load_percent = (class_taken[0][0]/class_scheduled[0][0])*100
+    work_load_percent = (class_taken[0][0]/class_scheduled[0][0])*100 #### studnet atten.
     doc.work_load_percent = "%.2f" % work_load_percent
 
     count = 0
@@ -29,8 +24,6 @@ def validate(doc,method):
         count +=1
         sum = sum+(t.duration)
 
-    print("Number of other activities :  ",count)
-    print("Sum of Duration of Other Activities : ",sum)
     # number_of_other_activities = frappe.db.sql("""select count(*) from `tabNon Teaching Activities` where parent=%s""",doc.name)
     doc.number_of_other_activities = count
 
