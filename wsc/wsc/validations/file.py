2a3a23c57c0f63d04bcf6d7ed64e66734ad76d44
Subhajit-SOUL <subhajit.saha@soulunileaders.com>
2023-01-18 12:41:19 +0530

    remove print

--- wsc/wsc/validations/file.py
+++ wsc/wsc/validations/file.py
@@ -7,7 +7,6 @@ def validate(doc,method):
     file_size(doc)
     
 def file_size(doc):
-    print("\n\n\n\n\n")
     # public_file_path = frappe.get_site_path('public', 'files', file_name) #./erp.soulunileaders.com/public/files/logo.png
     base_path = os.path.realpath(get_files_path(is_private=doc.is_private))
     file_name=doc.file_url.split("/")[-1] 
@@ -19,7 +18,6 @@ def file_size(doc):
     for t in ext_list:
         if t==extstr:
             file_size = os.path.getsize(url)
-            print('file_size',file_size)
             if file_size > 200000:
                 os.remove(url)
                 return frappe.throw("Image Size cannot exceed 200KB. Please upload an Image of smaller size.")
