93a2fb90fac5e5ceea5179be286b8e5ffa3fbb54
SPriytesh <priytesh.shah@soulunileaders.com>
2023-02-09 17:32:48 +0530

    new_placement_tool

--- wsc/wsc/doctype/placement_tool/placement_tool.js
+++ wsc/wsc/doctype/placement_tool/placement_tool.js
@@ -1,138 +1,76 @@
-// frappe.ui.form.on('Placement tool',{
-// 	refresh:function(frm){
-// 		// disabling features in the tool (save, add row, delete row)
-// 		frm.disable_save();
-// 		frm.set_df_property('student_child_table', 'cannot_add_rows', true);
-// 		frm.set_df_property('student_child_table', 'cannot_delete_rows', true);
-// 		// setting filters
-// 		frm.set_query('placement_drive_name', function(){
-// 			return{
-// 				filters:{
-// 					"placement_company":frm.doc.company_name,
-// 					"academic_year": frm.doc.placement_batch_year
-// 				}
-// 			}
-// 		})
-// 		frm.set_query('round_of_placement', function(){
-// 			return{
-// 				query: "wsc.wsc.doctype.Placement tool.Placement tool.get_round_of_placement",
-// 				filters:{
-// 					"parent":frm.doc.placement_drive_name	
-// 				}
-// 			}
-// 		})
-// 	},
-// 	// placement_drive_name: function(frm){
-// 	// 	// test code
-// 	// 	if(1){
-// 	// 		frappe.call({
-// 	// 			method: 'wsc.wsc.doctype.Placement tool.Placement tool.get_placement_rounds.get_round_names',
-// 	// 			args:{
-// 	// 				drive_name: frm.doc.placement_drive_name,
-// 	// 			},
-// 	// 			callback: function(result){
-// 	// 				if(result.message){
-// 	// 					console.log(result);
-// 	// 				}
-// 	// 			}
-// 	// 		})
-// 	// 	}
-// 	// },
-// 	get_student: function(frm){
-// 		if(frm.doc.company_name && frm.doc.placement_batch_year && frm.doc.placement_drive_name && frm.doc.round_of_placement && frm.doc.date_of_round){
-// 			frappe.call({
-// 				method:"wsc.wsc.doctype.Placement tool.Placement tool.get_student",
-// 				args:{
-// 					company_name: frm.doc.company_name,
-// 					placement_year: frm.doc.placement_batch_year,
-// 					drive_name: frm.doc.placement_drive_name,
-// 					round_of_placement: frm.doc.round_of_placement,
-// 					date_of_round: frm.doc.date_of_round
-// 				},
-// 				callback: function(result){
-// 					if(result.message){
-// 						alert(result.message);
-// 						frappe.model.clear_table(frm.doc,'student_child_table')
-// 						(result.message).forEach(element => {
-// 							var childTable = frm.add_child('student_child_table')
-// 							childTable.ref_no;
-// 							childTable.student_no;
-// 							childTable.student_name;
-// 							childTable.program_name;
-// 							childTable.academic_year;
-// 							childTable.semesters;
-// 						})
-// 					}
-// 					frm.refresh()
-//                 	frm.refresh_field("student_child_table")
-// 				}
-// 			})
-// 		}
-// 	},
-
-// 	// test button code
-// 	test_btn: function(frm){
-// 		if(frm.doc.placement_drive_name){
-// 			frappe.call({
-// 				method: 'wsc.wsc.doctype.Placement tool.Placement tool.test_Student_Data',
-// 				args:{
-// 					drive_name: frm.doc.placement_drive_name
-// 				},
-// 				callback: function(result){
-// 					alert(result.message);
-// 					if(result.message){
-						
-// 					}
-// 				}
-// 			})
-// 		}
-// 	}
-// })
-
-
-frappe.ui.form.on('Placement tool',{
+frappe.ui.form.on('Placement tool', {
     refresh: function(frm){
-        // disabling features
-        frm.disable_save()
-        frm.set_df_property('student_child_table', 'cannot_add_rows', true)
-        frm.set_df_property('student_child_table', 'cannot_delete_rows', true)
+        frm.disable_save();
+        frm.set_df_property('student_list', 'cannot_add_rows', true);
+        frm.set_df_property('student_list', 'cannot_delete_rows', true);
 
-        // setting filters
         frm.set_query('placement_drive_name', function(){
             return{
                 filters:{
                     'placement_company': frm.doc.company_name,
-                    'academic_year': frm.doc.placement_batch_year
+                    'academic_year' : frm.doc.placement_batch_year
                 }
             }
         })
-    },
-    get_student: function(frm){
-        if(frm.doc.company_name && frm.doc.placement_batch_year && frm.doc.placement_drive_name){
-            frappe.call({
-                method: 'wsc.wsc.doctype.Placement tool.Placement tool.get_student',
-                args:{
-                    drive_name:frm.doc.placement_drive_name
-                },
-                callback: function(result){
-                    if(result.message){
-                        alert(result.message)
-                    }
-                }
-            })
+        if(!frm.doc.__isLocal){
+            frm.add_custom_button(__('Schedule Round'), function(){
+                frappe.call({
+                    method: 'schedule_round',
+                    doc: frm.doc
+                })
+            }).addClass('btn-primary')
         }
     },
-    test_button: function(frm){
-        if(frm.doc.company_name && frm.doc.placement_batch_year && frm.doc.placement_drive_name){
-            frappe.call({
-                method:'wsc.wsc.doctype.Placement tool.Placement tool.test_button',
-                args:{
-                    drive_name : frm.doc.placement_drive_name
-                },
-                callback: function(result){
-                    alert(result)
+    placement_drive_name: function(frm){
+        frappe.call({
+            method: 'wsc.wsc.doctype.placement_tool.placement_tool.get_placement_rounds',
+            args:{
+                drive_name: frm.doc.placement_drive_name
+            },
+            callback: function(result){
+                let arr = [];
+                for(let x in result.message){
+                    arr.push(result.message[x]);
                 }
-            })
-        }
+                frm.set_df_property('round_of_placement', 'options', arr)
+            }
+        })
+    },
+    round_of_placement: function(frm){
+        frappe.call({
+            method:'wsc.wsc.doctype.placement_tool.placement_tool.get_date_of_round',
+            args:{
+                doc:frm.doc,
+                drive_name: frm.doc.placement_drive_name,
+                round_name: frm.doc.round_of_placement
+            },
+            callback: function(result){
+                frm.set_value('scheduled_date_of_round', result.message[0])
+            }
+        })
+    },
+    get_eligible_students_list: function(frm){
+        frappe.call({
+            method: 'wsc.wsc.doctype.placement_tool.placement_tool.get_eligible_students',
+            args:{
+                drive_name: frm.doc.placement_drive_name
+            },
+            callback:function(result){
+                if(result.message){
+                    frappe.model.clear_table(frm.doc, 'student_list');
+                    result.message.forEach(elements => {
+                        var childTable = frm.add_child('student_list');
+                        childTable.ref_no = element.name
+                        childTable.student_no = element.student
+                        childTable.student_name = element.student_name
+                        childTable.program_name = element.programs
+                        childTable.academic_year = element.academic_year
+                        childTable.semesters = element.semesters
+                    })
+                }
+                frm.refresh()
+                frm.refresh_field('student_list')
+            }
+        })
     }
 })
---
+++ wsc/wsc/doctype/placement_tool/placement_tool.py
@@ -0,0 +1,55 @@
+# Copyright (c) 2023, SOUL Limited and Contributors
+# For license information, please see license.txt
+
+# import frappe
+# from frappe.model.document import Document
+# from frappe.model.mapper import get_mapped_doc
+
+# class Placementtool(Document):
+# 	@frappe.whitelist()
+#     def schedule_round(self):
+#         result = frappe.new_doc('Selection Round')
+
+
+import frappe
+from frappe.model.document import Document
+from frappe.model.mapper import get_mapped_doc
+
+class Placementtool(Document):
+    @frappe.whitelist()
+    def schedule_round(self):
+        for d in self.get('student_list'):
+            result = frappe.new_doc('Selection Round')
+            result.student_name = d.student_name
+            result.student_no = d.student_no
+            result.program_name = d.program_name
+            result.academic_year = d.academic_year
+            result.semesters = d.semesters
+            result.company_name = self.company_name
+            result.placement_batch_year = self.placement_batch_year
+            result.placement_drive_name = self.placement_drive_name
+            result.round_of_placement = self.round_of_placement
+            result.scheduled_date_of_round = self.scheduled_date_of_round
+            result.save()
+            result.submit()
+
+
+@frappe.whitelist()
+def get_placement_rounds(drive_name):
+    data = frappe.db.sql(""" SELECT round_name from `tabRounds of Placement` where parent = "%s" """%(drive_name))
+    return data
+
+@frappe.whitelist()
+def get_date_of_round(doc, drive_name, round_name):
+    data = frappe.db.sql(""" SELECT date from `tabRounds of Placement` where parent = '%s' AND round_name = '%s'"""%(drive_name, round_name))
+    return data
+
+frappe.whitelist()
+def get_eligible_students_list(drive_name):
+    student_data = frappe.get_all('Placement Drive Application', [['placement_drive', '=', drive_name], ['status', '=', 'Shortlisted']], ['student', 'name', 'student_name'])
+    for t in student_data:
+        data = frappe.get_all('Current Educational Details', {'parent':t['student'], 'parenttype': 'student'}, ['programs', 'semesters', 'academic_year'])
+        t['programs'] = data[0]['programs']
+        t['semesters'] = data[0]['semesters']
+        t['academic_year'] = data[0]['academic_year']
+    return student_data
