329a7fbf0263fa009afc30fd2aec9982b8510bbb
SayantanGhosh22 <sayantan.ghosh@soulunileaders.com>
2023-05-30 12:12:46 +0530

    Placement tool and entrepreneurship screens

--- wsc/wsc/doctype/placement_tool/placement_tool.py
+++ wsc/wsc/doctype/placement_tool/placement_tool.py
@@ -1,49 +1,163 @@
-# Copyright (c) 2023, SOUL Limited and contributors
-# For license information, please see license.txt
+# # Copyright (c) 2023, SOUL Limited and contributors
+# # For license information, please see license.txt
 
-import frappe
+
+import frappe 
 from frappe.model.document import Document
 from frappe.model.mapper import get_mapped_doc
 
-
-
 class PlacementTool(Document):
-	@frappe.whitelist()
-	def schedule_round(self):
-		if(self.round_status == 'Scheduling Of Round'):
-			status = frappe.db.sql(""" SELECT COUNT(*) FROM `tabSelection Round` WHERE placement_drive_name = '%s' AND round_of_placement = '%s' """%(self.placement_drive_name, self.round_of_placement))
-			data = frappe.db.sql(""" SELECT round_name from `tabRounds of Placement` where parent="%s" ORDER BY idx ASC"""%(self.placement_drive_name))
-			data = data[-1]
-			print('\n\n\n\n', status[0][0],'\n\n\n\n')
-			if(status[0][0] == 0):
-				for d in self.get('student_list'):
-					result = frappe.new_doc('Selection Round')
-					result.student_name = d.student_name
-					result.student_no = d.student_no
-					result.program_name = d.program_name
-					result.academic_year = d.academic_year
-					result.semesters = d.semesters
-					result.company_name = self.company_name
-					result.placement_batch_year = self.placement_batch_year
-					result.drive_title = self.drive_title
-					result.placement_drive_name = self.placement_drive_name					
-					result.round_of_placement = self.round_of_placement				
-					result.scheduled_date_of_round = self.scheduled_date_of_round
-					if(data == self.round_of_placement):
-						result.shortlisting_status = 'Interview scheduled'
-					else:
-						result.shortlisting_status = d.shortlisting_status
-					result.save()
-					result.submit()
-				frappe.db.sql(""" UPDATE `tabRounds of Placement` SET round_status = 'Round Scheduled' WHERE parent = '%s' and round_name = '%s' """%(self.placement_drive_name, self.round_of_placement))
-			elif(status[0][0] > 0):
-				print('\n\n\n\n Record already exists \n\n\n\n')
-		elif(self.round_status == 'Round Result Declaration'):
-			print('\n\n\n\n','Elif','\n\n\n\n')
-			for d in self.get('student_list'):
-				frappe.db.sql(""" UPDATE `tabSelection Round` SET shortlisting_status = '%s' WHERE student_no='%s' AND placement_drive_name = '%s' AND placement_batch_year='%s' AND round_of_placement = '%s'"""%(d.shortlisting_status, d.student_no,self.placement_drive_name, self.placement_batch_year, self.round_of_placement))
-				frappe.db.sql(""" UPDATE `tabPlacement Drive Application` SET status = '%s' WHERE student = '%s' AND placement_drive = '%s' """%(d.shortlisting_status, d.student_no, self.placement_drive_name))
-				frappe.db.sql(""" UPDATE `tabApplied Companies` SET round_status = '%s' WHERE parent = '%s' AND drive_title = '%s' """%(d.shortlisting_status, d.student_no, self.drive_title))
+    @frappe.whitelist()
+    def declare_round(self, round_status, drive_name, round_name):
+        if(round_status == 'Scheduling Of Round'):
+            set_round_status = frappe.db.sql(""" UPDATE `tabRounds of Placement` SET round_status = 'Round Scheduled' WHERE parent = '%s' and round_name = '%s' """%(drive_name, round_name))
+            pass
+        elif(round_status == 'Round Result Declaration'):
+            set_round_status = frappe.db.sql(""" UPDATE `tabRounds of Placement` SET round_status = 'Round Scheduled' WHERE parent = '%s' and round_name = '%s' """%(drive_name, round_name))
+            
+        for d in self.get('student_records'):
+             result = frappe.new_doc('Selection Round')
+             result.student_name = d.student_name
+             result.student_no = d.student_no
+             result.program_name = d.program_name
+             result.academic_year = d.academic_year
+             result.semesters = d.semesters
+             result.company_name = self.company_name
+             result.placement_batch_year = self.placement_batch_year
+             result.placement_drive_name = self.placement_drive_name
+             result.round_of_placement = self.round_of_placement
+             result.scheduled_date_of_round = self.scheduled_date_of_round
+             result.save()
+             result.submit()
+    
+@frappe.whitelist()
+def get_drive_names(company_name):
+    data = frappe.db.sql(""" SELECT title FROM `tabPlacement Drive` WHERE placement_company = '%s'"""%(company_name))
+    return data
+
+@frappe.whitelist()
+def get_placement_round_names(self, drive_name, round_status):
+    all_round_names = frappe.db.sql(""" SELECT idx, round_name FROM `tabRounds of Placement` WHERE parent = '%s' ORDER BY idx ASC """ %(drive_name))
+    rounds_scheduled = frappe.db.sql(""" SELECT idx, round_name FROM `tabRounds of Placement` WHERE parent = '%s' AND round_status = 'Scheduled' ORDER BY idx ASC"""%(drive_name))
+    rounds_not_scheduled = frappe.db.sql(""" SELECT idx, round_name FROM `tabRounds of Placement` WHERE parent = '%s' AND round_status = 'Not Scheduled' ORDER BY idx ASC"""%(drive_name))
+    rounds_result_declared = frappe.db.sql(""" SELECT idx, round_name FROM `tabRounds of Placement` WHERE parent = '%s' AND round_status = 'Result Declared' ORDER BY idx ASC"""%(drive_name))
+    if(round_status == 'Scheduling Of Round'):
+        if(len(rounds_not_scheduled) == 0):
+            return 'all rounds have been scheduled'
+        elif(len(rounds_not_scheduled) > 0):
+            return rounds_not_scheduled[0][1]
+    elif(round_status == 'Round Result Declaration'):
+        if(len(rounds_scheduled) == 0):
+            return 'No rounds have been scheduled'
+        elif(len(rounds_result_declared) == len(all_round_names)):
+            return 'All results have been declared'
+        elif(len(rounds_scheduled) > 0):
+            return rounds_scheduled[0][1]
+
+@frappe.whitelist()
+def get_round_details(doc, drive_name, round_name):
+    data = frappe.db.sql(""" SELECT date , reporting_time from `tabRounds of Placement` where parent = '%s' AND round_name = '%s'"""%(drive_name, round_name))
+    return data
+
+@frappe.whitelist()
+def get_students(drive_name):
+    fil = []
+    fil.append(['placement_drive', '=', drive_name])
+    fil.append(['status', 'in',['Applied','Shortlisted']])
+    fil.append(['docstatus','=',1])
+    student_data = frappe.get_all('Placement Drive Application', filters=fil, fields=['student','name','student_name'])
+    for t in student_data:
+        data = frappe.get_all('Current Educational Details',{'parent':t['student'], 'parenttype':'student'},['programs','semesters', 'academic_year'])
+        t['programs'] = data[0]['programs']
+        t['semesters'] = data[0]['semesters']
+        t['academic_year'] = data[0]['academic_year']
+    return student_data
+
+
+# import frappe
+# from frappe.model.document import Document
+# from frappe.model.mapper import get_mapped_doc
+
+
+# class PlacementTool(Document):
+#     @frappe.whitelist()
+#     # creates records in selection round doctype
+#     def schedule_round(self):
+#         pass
+
+# @frappe.whitelist()
+# # gets the eligible students from the placement drive
+# def get_student(drive_name):
+#     pass
+
+# @frappe.whitelist()
+# # get date and time of the round
+# def get_date_of_round(doc, drive_name, round_name):
+#     pass
+
+# @frappe.whitelist()
+# # get title of the round
+# def get_title(company_name):
+#     data = frappe.db.sql(""" SELECT title FROM `tabPlacement Drive` WHERE placement_company = '%s'"""%(company_name))
+#     return data
+
+# # @frappe.whitelist()
+# # # get rounds of placement
+# # def get_rounds_of_placement(self, drive_name = None, round_status = None):
+# #     all_round_names = frappe.db.sql()
+# #     rounds_scheduled = frappe.db.sql()
+# #     rounds_not_scheduled = frappe.db.sql()
+# #     # for result declaration the last value of the rounds_scheduled will be taken
+# # 	# for round scheduling the first value of rounds_not_scheduled will be taken
+# #     if(self.round_status == 'Scheduling Of Round'):
+# #         round_tobe_scheduled_idx = rounds_not_scheduled[0][0]
+# #         round_tobe_scheduled_name = rounds_not_scheduled[0][1]
+# #         if('1'):
+            
+             
+	
+    
+    
+
+
+
+# # class PlacementTool(Document):
+# # 	@frappe.whitelist()
+# # 	def schedule_round(self):
+# # 		if(self.round_status == 'Scheduling Of Round'):
+# # 			status = frappe.db.sql(""" SELECT COUNT(*) FROM `tabSelection Round` WHERE placement_drive_name = '%s' AND round_of_placement = '%s' """%(self.placement_drive_name, self.round_of_placement))
+# # 			data = frappe.db.sql(""" SELECT round_name from `tabRounds of Placement` where parent="%s" ORDER BY idx ASC"""%(self.placement_drive_name))
+# # 			data = data[-1]
+# # 			print('\n\n\n\n', status[0][0],'\n\n\n\n')
+# # 			if(status[0][0] == 0):
+# # 				for d in self.get('student_list'):
+# # 					result = frappe.new_doc('Selection Round')
+# # 					result.student_name = d.student_name
+# # 					result.student_no = d.student_no
+# # 					result.program_name = d.program_name
+# # 					result.academic_year = d.academic_year
+# # 					result.semesters = d.semesters
+# # 					result.company_name = self.company_name
+# # 					result.placement_batch_year = self.placement_batch_year
+# # 					result.drive_title = self.drive_title
+# # 					result.placement_drive_name = self.placement_drive_name					
+# # 					result.round_of_placement = self.round_of_placement				
+# # 					result.scheduled_date_of_round = self.scheduled_date_of_round
+# # 					if(data == self.round_of_placement):
+# # 						result.shortlisting_status = 'Interview scheduled'
+# # 					else:
+# # 						result.shortlisting_status = d.shortlisting_status
+# # 					result.save()
+# # 					result.submit()
+# # 				frappe.db.sql(""" UPDATE `tabRounds of Placement` SET round_status = 'Round Scheduled' WHERE parent = '%s' and round_name = '%s' """%(self.placement_drive_name, self.round_of_placement))
+# # 			elif(status[0][0] > 0):
+# # 				print('\n\n\n\n Record already exists \n\n\n\n')
+# # 		elif(self.round_status == 'Round Result Declaration'):
+# # 			print('\n\n\n\n','Elif','\n\n\n\n')
+# # 			for d in self.get('student_list'):
+# # 				frappe.db.sql(""" UPDATE `tabSelection Round` SET shortlisting_status = '%s' WHERE student_no='%s' AND placement_drive_name = '%s' AND placement_batch_year='%s' AND round_of_placement = '%s'"""%(d.shortlisting_status, d.student_no,self.placement_drive_name, self.placement_batch_year, self.round_of_placement))
+# # 				frappe.db.sql(""" UPDATE `tabPlacement Drive Application` SET status = '%s' WHERE student = '%s' AND placement_drive = '%s' """%(d.shortlisting_status, d.student_no, self.placement_drive_name))
+# # 				frappe.db.sql(""" UPDATE `tabApplied Companies` SET round_status = '%s' WHERE parent = '%s' AND drive_title = '%s' """%(d.shortlisting_status, d.student_no, self.drive_title))
 
 
 @frappe.whitelist()
@@ -63,53 +177,80 @@ def get_student(drive_name):
         t['academic_year'] = data[0]['academic_year']
     return student_data
 
-@frappe.whitelist()
-def get_date_of_round(doc, drive_name, round_name):
-	data = frappe.db.sql(""" SELECT date , reporting_time from `tabRounds of Placement` where parent = '%s' AND round_name = '%s'"""%(drive_name, round_name))
-	return data
+# # @frappe.whitelist()
+# # def get_date_of_round(doc, drive_name, round_name):
+# # 	data = frappe.db.sql(""" SELECT date , reporting_time from `tabRounds of Placement` where parent = '%s' AND round_name = '%s'"""%(drive_name, round_name))
+# # 	return data
 
-@frappe.whitelist()
-def get_title(company_name):
-	data = frappe.db.sql(""" SELECT title FROM `tabPlacement Drive` where placement_company = '%s' """%(company_name))
-	return data
+# # @frappe.whitelist()
+# # def get_title(company_name):
+# # 	data = frappe.db.sql(""" SELECT title FROM `tabPlacement Drive` where placement_company = '%s' """%(company_name))
+# # 	return data
 
-@frappe.whitelist()
-def get_rounds_of_placement(self,drive_name=None,round_status=None):
-	print('echo')
-	all_round_names = frappe.db.sql(""" SELECT idx, round_name FROM `tabRounds of Placement` WHERE parent = '%s' ORDER BY idx ASC"""%(self.placement_drive_name))
-	rounds_not_scheduled = frappe.db.sql(""" SELECT idx, round_name FROM `tabRounds of Placement` WHERE parent = '%s' AND round_status = 'Not Scheduled' ORDER BY idx ASC"""%(self.placement_drive_name))
-	rounds_scheduled = frappe.db.sql(""" SELECT idx, round_name FROM `tabRounds of Placement` WHERE parent = '%s' AND round_status = 'Scheduled' ORDER BY idx """%(self.placement_drive_name))
+
+# @frappe.whitelist()
+# def get_rounds_of_placement(drive_name = None, round_status = None):
+#     all_round_names = frappe.db.sql(""" SELECT idx, round_name FROM `tabRounds of Placement` WHERE parent = '%s' ORDER BY idx ASC """ %(drive_name))
+#     rounds_not_scheduled = frappe.db.sql(""" SELECT idx, round_name FROM `tabRounds of Placement` WHERE parent = '%s' AND round_status = 'Not Scheduled' ORDER BY idx ASC"""%(drive_name))
+#     rounds_scheduled = frappe.db.sql(""" SELECT idx, round_name FROM `tabRounds of Placement` WHERE parent = '%s' AND round_status = 'Scheduled' ORDER BY idx """%(drive_name))
+#     round_results_declared = frappe.db.sql(""" SELECT idx, round_name FROM `tabRounds of Placement` WHERE parent = '%s' AND round_status = 'Result Declared' ORDER BY idx """%(drive_name))
+#     if(round_status == 'Scheduling Of Round'):
+#         print(rounds_not_scheduled)
+#         return rounds_not_scheduled[0][1]
+#     elif(round_status == 'Round Result Declaration'):
+#         if(len(rounds_scheduled) != 0):
+#             return 'round 9'
+#     if(round_status == 'Scheduling Of Round'):
+#         if(len(rounds_not_scheduled) == 0):
+#             return 'All rounds have been scheduled'
+#         elif(len(rounds_not_scheduled) != 0):
+#             return rounds_not_scheduled[0][1]
+#     elif(round_status == 'Round Result Declaration'):
+#         if(len(rounds_scheduled) == 0):
+#             return 'No round has been scheduled'
+#         elif(len(round_results_declared) == len(all_round_names)):
+#             return 'All results have been declared'
+#         elif(len(rounds_scheduled) != 0):
+#             return 'round 9'
+        
+
+# # @frappe.whitelist()
+# # def get_rounds_of_placement(self,drive_name=None,round_status=None):
+# # 	print('echo')
+# # 	all_round_names = frappe.db.sql(""" SELECT idx, round_name FROM `tabRounds of Placement` WHERE parent = '%s' ORDER BY idx ASC"""%(drive_name))
+# # 	rounds_not_scheduled = frappe.db.sql(""" SELECT idx, round_name FROM `tabRounds of Placement` WHERE parent = '%s' AND round_status = 'Not Scheduled' ORDER BY idx ASC"""%(drive_name))
+# # 	rounds_scheduled = frappe.db.sql(""" SELECT idx, round_name FROM `tabRounds of Placement` WHERE parent = '%s' AND round_status = 'Scheduled' ORDER BY idx """%(drive_name))
 	
-	previous_round = rounds_scheduled[0]
-	previous_round_idx = previous_round[0]
-	previous_round_name = previous_round[1]
+# # 	previous_round = rounds_scheduled[0]
+# # 	previous_round_idx = previous_round[0]
+# # 	previous_round_name = previous_round[1]
 	
-	current_round = rounds_not_scheduled[0]
-	current_round_idx = current_round[0]
-	current_round_name = current_round[1]
+# # 	current_round = rounds_not_scheduled[0]
+# # 	current_round_idx = current_round[0]
+# # 	current_round_name = current_round[1]
 
-	current_round_scheduling_status = frappe.db.sql(""" SELECT COUNT(*) FROM `tabSelection Round` parent = '%s' AND round_of_placement = '%s' AND round_status = 'scheduled'"""%(self.placement_drive_name, current_round_name))
+# # 	current_round_scheduling_status = frappe.db.sql(""" SELECT COUNT(*) FROM `tabSelection Round` parent = '%s' AND round_of_placement = '%s' AND round_status = 'scheduled'"""%(drive_name, current_round_name))
 
-	if(self.round_status == 'Scheduling Of Round'):
-		if(current_round_idx == 1):
-			if(current_round_scheduling_status[0][0] == 0):
-				return current_round_name
-		elif(current_round_idx > 1):
-			if(current_round_scheduling_status[0][0] == 0):
-				previous_round_result_status = frappe.db.sql(""" SELECT COUNT(*) FROM `tabSelection Round` parent = '%s' AND round_of_placement = '%s' AND round_status = 'result declared'"""%(self.placement_drive_name, previous_round_name))
-				if(previous_round_result_status > 0):
-					return current_round_name
-	elif(self.round_status == 'Round Result Declaration'):
-		if(current_round_scheduling_status[0][0] > 1):
-			current_round_result_status = frappe.db.sql(""" SELECT COUNT(*) FROM `tabSelection Round` parent = '%s' AND round_of_placement = '%s' AND round_status = 'result declared'"""%(self.placement_drive_name, current_round_name))
-			previous_round_result_status = frappe.db.sql(""" SELECT COUNT(*) FROM `tabSelection Round` parent = '%s' AND round_of_placement = '%s' AND round_status = 'result declared'"""%(self.placement_drive_name, previous_round_name))
-			if(current_round_idx == 1):
-				if(current_round_result_status[0][0] == 0):
-					if(current_round_scheduling_status[0][0] > 0):
-						return current_round_name
-			elif(current_round_idx > 1):
-				if(current_round_result_status[0][0] == 0):
-					if(current_round_scheduling_status[0][0] > 1):
-						previous_round_result_status = frappe.db.sql(""" SELECT COUNT(*) FROM `tabSelection Round` parent = '%s' AND round_of_placement = '%s' AND round_status = 'result declared'"""%(self.placement_drive_name, previous_round_name))
-						if(previous_round_result_status[0][0] > 0):
-							return current_round_name
+# # 	if(self.round_status == 'Scheduling Of Round'):
+# # 		if(current_round_idx == 1):
+# # 			if(current_round_scheduling_status[0][0] == 0):
+# # 				return current_round_name
+# # 		elif(current_round_idx > 1):
+# # 			if(current_round_scheduling_status[0][0] == 0):
+# # 				previous_round_result_status = frappe.db.sql(""" SELECT COUNT(*) FROM `tabSelection Round` parent = '%s' AND round_of_placement = '%s' AND round_status = 'result declared'"""%(drive_name, previous_round_name))
+# # 				if(previous_round_result_status > 0):
+# # 					return current_round_name
+# # 	elif(self.round_status == 'Round Result Declaration'):
+# # 		if(current_round_scheduling_status[0][0] > 1):
+# # 			current_round_result_status = frappe.db.sql(""" SELECT COUNT(*) FROM `tabSelection Round` parent = '%s' AND round_of_placement = '%s' AND round_status = 'result declared'"""%(drive_name, current_round_name))
+# # 			previous_round_result_status = frappe.db.sql(""" SELECT COUNT(*) FROM `tabSelection Round` parent = '%s' AND round_of_placement = '%s' AND round_status = 'result declared'"""%(drive_name, previous_round_name))
+# # 			if(current_round_idx == 1):
+# # 				if(current_round_result_status[0][0] == 0):
+# # 					if(current_round_scheduling_status[0][0] > 0):
+# # 						return current_round_name
+# # 			elif(current_round_idx > 1):
+# # 				if(current_round_result_status[0][0] == 0):
+# # 					if(current_round_scheduling_status[0][0] > 1):
+# # 						previous_round_result_status = frappe.db.sql(""" SELECT COUNT(*) FROM `tabSelection Round` parent = '%s' AND round_of_placement = '%s' AND round_status = 'result declared'"""%(drive_name, previous_round_name))
+# # 						if(previous_round_result_status[0][0] > 0):
+# # 							return current_round_name
