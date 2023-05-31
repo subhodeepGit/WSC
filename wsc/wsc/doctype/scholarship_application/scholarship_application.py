7c9cfa26cc62622b6d94913265d4aae926cacd90
abhishekadhikari88 <aadhikari@kiit.ac.in>
2023-05-31 15:52:39 +0530

    update in scholarship

--- wsc/wsc/doctype/scholarship_application/scholarship_application.py
+++ wsc/wsc/doctype/scholarship_application/scholarship_application.py
@@ -6,7 +6,9 @@ from datetime import date
 from frappe.model.document import Document
 
 class ScholarshipApplication(Document):
-	pass
+	def validate(self):
+		if len(self.document_list_tab) == 0:     
+			add_document_list_rows(self)
 
 
 @frappe.whitelist()
@@ -24,3 +26,26 @@ def current_education(student_no):
 	current_education_data=frappe.get_all("Current Educational Details",{"parent":student_no},['programs','semesters','academic_year','academic_term'])
 	return current_education_data
 
+
+def add_document_list_rows(self): 
+	if self.student_catagory and self.academic_year:
+		self.set("document_list_tab",[])
+	# get_document_list_by_category(self)
+        # for d in get_document_list_by_category(doc):
+        #     doc.append("document_list",{
+        #         "document_name":d.document_name,
+        #         "mandatory":d.mandatory,
+        #         "is_available" :d.is_available
+        #     })
+
+def get_document_list_by_category(self):
+    filters={"student_category":self.student_category}
+    group_by=""
+    # if doc.counselling_structure:
+    #     filters.update({"parent":doc.counselling_structure,"parenttype":"Counselling Structure"})
+    # else:
+    #     filters.update({"parent":["IN",[d.student_admission for d in doc.get('program_priority')]],"parenttype":"Student Admission"})
+    #     group_by="document_name"
+    # doc_list  = frappe.db.sql("""SELECT DL.document_name, DL.mandatory, DL.is_available from `tabDocuments Template List` as DL 
+    # inner join `tabDocuments Template` as D on DL.parent= D.name where D.student_category='{0}' and D.academic_year = '{1}' ORDER BY document_name ASC""".format(doc.student_category,doc.academic_year) ,as_dict=1)
+    # return doc_list if doc_list else []	    
