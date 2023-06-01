b13d0f7c111f91e0864988b36a5a4a2cafa4e463
SayantanGhosh22 <sayantan.ghosh@soulunileaders.com>
2023-06-01 18:23:35 +0530

    Training module code

--- wsc/wsc/doctype/event_feedback/event_feedback.js
+++ wsc/wsc/doctype/event_feedback/event_feedback.js
@@ -2,7 +2,40 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Event feedback', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
 
-	// }
+	},
+	participant_id : function(frm){
+		frappe.call({
+			method :'wsc.wsc.doctype.event_feedback.event_feedback.get_participant_name',
+			args:{
+				participant_id : frm.doc.participant_id
+			},
+			callback: function(result){
+				frm.set_value('participant_name', result.message)
+			}
+		})
+	},
+	select_program : function(frm){
+		frappe.call({
+			method : 'wsc.wsc.doctype.event_feedback.event_feedback.get_program_name',
+			args: {
+				program_id : frm.doc.select_program
+			},
+			callback : function(result){
+				frm.set_value('program_name', result.message)
+			}
+		})
+	},
+	select_event : function(frm){
+		frappe.call({
+			method: 'wsc.wsc.doctype.event_feedback.event_feedback.get_event_name',
+			args:{
+				event_id : frm.doc.select_event
+			},
+			callback : function(result){
+				frm.set_value("event_name", result.message)
+			}
+		})
+	}
 });
--- wsc/wsc/doctype/participant_attendance/participant_attendance.py
+++ wsc/wsc/doctype/participant_attendance/participant_attendance.py
@@ -1,8 +1,36 @@
 # Copyright (c) 2023, SOUL Limited and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe 
 from frappe.model.document import Document
+from frappe.model.mapper import get_mapped_doc
 
 class ParticipantAttendance(Document):
 	pass
+
+@frappe.whitelist()
+def get_program_name(program_id):
+	program_name = frappe.db.sql("""SELECT program_name FROM `tabTnP Program` WHERE name = '%s'"""%(program_id))
+	print('\n\n\n\n')
+	print(program_name[0][0])
+	print("\n\n\n\n")
+	return program_name[0][0]
+
+@frappe.whitelist()
+def get_event_name(event_id):
+	event_details = frappe.db.sql(""" SELECT event_name FROM `tabTnP Event` WHERE name = '%s' """%(event_id))
+	print('\n\n\n\n')
+	print(event_details[0][0])
+	print('\n\n\n\n')
+	return event_details[0][0]
+
+@frappe.whitelist()
+def get_participants(event_id):
+	participant_data = frappe.get_all('Participant Registration', filters = [['select_event', '=', event_id]], fields = ['participant_id'])
+	for t in participant_data:
+		student_name = frappe.get_all('Student', filters = [['name', '=', t['participant_id']]], fields = ['student_name'])
+		t['student_name'] = student_name[0]['student_name']
+	print('\n\n\n\n')
+	print(participant_data)
+	print('\n\n\n\n')
+	return participant_data
