db1519db05f4a5c6154d554f3e05d795b33deef0
abhishekadhikari88 <aadhikari@kiit.ac.in>
2023-06-01 18:12:16 +0530

    update in scholarship

--- wsc/wsc/doctype/scholarship_application/scholarship_application.js
+++ wsc/wsc/doctype/scholarship_application/scholarship_application.js
@@ -2,9 +2,33 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Scholarship Application', {
+	hide_n_show_child_table_fields(frm){
+		var df = frappe.meta.get_docfield("Scholarship Eligibility Paramete","parameter", frm.doc.name);
+        df.read_only = 1
+		var df = frappe.meta.get_docfield("Scholarship Eligibility Paramete","percentagecgpa", frm.doc.name);
+        df.read_only = 1
+	},
+	before_load: function(frm) {
+        frm.trigger("hide_n_show_child_table_fields");
+    },
 	refresh: function(frm) {
 		frm.set_df_property('document_list_tab', 'cannot_add_rows', true);
+		frm.set_df_property('scholarship_eligibility_parameter', 'cannot_add_rows', true);
 	},
+
+
+
+	// setup:function(frm){
+	// 	frm.set_query("scholarship_id", function() {
+	// 		return {
+	// 			query:"wsc.wsc.doctype.scholarship_application.scholarship_application.valid_scholarship",
+	// 			// filters: {
+	// 			// 	"docstatus": 1,
+	// 			// }
+	// 		}
+	// 	})	
+	// },
+
 	student_id: function(frm) {
 		frappe.call({
 			method: 'wsc.wsc.doctype.scholarship_application.scholarship_application.calculateAge',
@@ -37,6 +61,25 @@ frappe.ui.form.on('Scholarship Application', {
 				}
 			},
 
+		})
+	},
+	scholarship_id: function(frm){
+		frappe.call({
+			method: 'wsc.wsc.doctype.scholarship_application.scholarship_application.eligibility',
+			args:{
+				"scholarship_id_data":frm.doc.scholarship_id
+			},
+			callback: function(r) {
+				if (r.message){
+					frappe.model.clear_table(frm.doc, 'scholarship_eligibility_parameter');
+					(r.message).forEach(element => {
+						var c = frm.add_child("scholarship_eligibility_parameter")
+						c.parameter=element.parameter
+						c.percentagecgpa=element.percentagecgpa
+					});
+					frm.refresh_field("scholarship_eligibility_parameter")
+				}
+			}
 		})
 	}
 });
