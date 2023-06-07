c7a0542469ae987883b87aa468a3528da7d7e605
Subhajit-SOUL <subhajit.saha@soulunileaders.com>
2023-06-07 14:40:38 +0530

    remove print

--- wsc/wsc/doctype/fees_waiver/fees_waiver.py
+++ wsc/wsc/doctype/fees_waiver/fees_waiver.py
@@ -8,6 +8,5 @@ from frappe import _
 class FeesWaiver(Document):
 	def validate(self):
 		existing_data=frappe.get_all("Fees Waiver",filters={"student":self.student,"fee_type":self.fee_type,"programs":self.programs,"semester":self.semester,"academic_year":self.academic_year,"academic_term":self.academic_term,"docstatus":1},fields=["name"])
-		print(existing_data)
 		if existing_data:
 			frappe.throw(_("Fees Waiver already exists {0}").format(frappe.bold(existing_data[0]["name"])))
