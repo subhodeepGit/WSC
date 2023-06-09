1e06cb624d0f9714358a97cf3422c3fdfc762483
SarthakRath <sarthak.srath@soulunileaders.com>
2023-06-09 13:14:24 +0530

    commiting employee employee_reengagement

--- wsc/wsc/doctype/employee_reengagement/employee_reengagement.py
+++ wsc/wsc/doctype/employee_reengagement/employee_reengagement.py
@@ -6,18 +6,39 @@ from frappe.model.document import Document
 from frappe import _,get_list
 
 class EmployeeReengagement(Document):
+	
 	pass
+
 @frappe.whitelist()
 def isrfp(docname):
+	# if frappe.db.exists(docname):
+
 	doc = frappe.get_doc("Employee Reengagement",docname)
+	print("\n\n\n\n\n\n")
+	print(doc.employee)
+	print("\n\n\n\n\n\n")
+	emp_user_id = frappe.get_all("Employee",{"name":doc.employee},["user_id"])
+	if emp_user_id:
+		employee_user_id = emp_user_id[0]["user_id"]
 	reporting_auth = doc.reporting_authority
 	reporting_auth_id = frappe.get_all("Employee",{"name":reporting_auth},["user_id"])
 	# print("reporting_auth_id",reporting_auth_id)
 	if reporting_auth_id:
 		reporting_auth_id=reporting_auth_id[0]["user_id"]
-		if reporting_auth_id==frappe.session.user :
-			return True
+	roles = frappe.get_roles(frappe.session.user)
+	if "HR Manager/CS Officer" in roles or "Department Head" in roles or "HR Admin" in roles or "Director" in roles:
+		return True
+	if "Employee" in roles and doc.workflow_state=="Draft" and frappe.session.user==employee_user_id:
+		return True
+	if reporting_auth_id==frappe.session.user and doc.workflow_state=="Pending Approval from Reporting Authority":
+		return True
+	else :
+		return False
+	# else :
+	# 	pass
 		
+
+
 
 		
 
