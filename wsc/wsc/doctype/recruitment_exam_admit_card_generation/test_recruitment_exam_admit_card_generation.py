99d1acfe275be0b59d34ccffd33fb150c983ecbf
SNSoul414 <sukalyan.nayak@soulunileaders.com>
2023-06-12 10:41:01 +0530

    commiting

--- wsc/wsc/doctype/recruitment_exam_admit_card_generation/test_recruitment_exam_admit_card_generation.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, SOUL Limited and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestRecruitmentExamAdmitCardGeneration(FrappeTestCase):
-	pass
