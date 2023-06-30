ff54572125df96480fce76f4d03d244aa8aab548
abhishekadhikari88 <aadhikari@kiit.ac.in>
2023-06-30 17:33:43 +0530

    update in Module wise gr.

--- wsc/wsc/doctype/module_wise_exam_group/module_wise_exam_group.py
+++ wsc/wsc/doctype/module_wise_exam_group/module_wise_exam_group.py
@@ -3,7 +3,7 @@
 
 import frappe
 from frappe.model.document import Document
-from datetime import datetime
+from datetime import datetime, timedelta
 from wsc.wsc.notification.custom_notification import send_mail_to_students_mweg,send_mail_to_trainers_mweg
 
 
@@ -15,13 +15,13 @@ class ModuleWiseExamGroup(Document):
 		date_validation(self)
 		validate_time(self)
 		self.calculate_total_hours()
+		over_lapping_of_scheduling(self)
 		
 
 	def on_submit(self):
 		group_validation(self,"on_submit")
 		date_time_mandatory(self)
 		time_mandatory(self)
-		over_lapping_of_scheduling(self) 
 		send_mail_to_students_mweg(self)
 		send_mail_to_trainers_mweg(self)
 	
@@ -31,27 +31,96 @@ class ModuleWiseExamGroup(Document):
 				d.total_duration_in_hours = datetime.strptime(d.to_time, '%H:%M:%S') - datetime.strptime(d.from_time, '%H:%M:%S') 	
 
 def over_lapping_of_scheduling(self):
-	print("\n\n\n")
-	student_list=[]
-	for t in self.get('student_list'):
-		student_list.append(t.student_no)
-	print(student_list)
-	if len(student_list)==1:	
-		student_gr_data=frappe.db.sql(""" select MWEG.name,MWES.student_no,MWES.group_name from `tabModule Wise Exam Group` MWEG
-											Join `tabModule Wise Exam Student` MWES ON MWEG.name=MWES.parent where MWES.student_no ='%s'
-											"""%(student_list[0]),as_dict=True)
-	else:
-		student_gr_data=frappe.db.sql(""" select MWEG.name,MWES.student_no,MWES.group_name from `tabModule Wise Exam Group` MWEG
-											Join `tabModule Wise Exam Student` MWES ON MWEG.name=MWES.parent where MWES.student_no IN %s 
-											"""%(str(tuple(student_list))),as_dict=True)
-	
+	if self.get('student_list') and self.get('scheduling_group_exam'):
+		student_list=[]
+		for t in self.get('student_list'):
+			student_list.append(t.student_no)
 
-	# for t in student_gr_data:
-	# 	pass
+		if len(student_list)==1:	
+			student_gr_data=frappe.db.sql(""" select MWEG.name,MWEG.exam_declaration_id,MWES.student_no,MWES.student_name, MWES.roll_no,MWES.group_name 
+												from `tabModule Wise Exam Group` MWEG
+												Join `tabModule Wise Exam Student` MWES ON MWEG.name=MWES.parent where MWES.student_no ='%s' 
+												and (MWEG.module_exam_start_date <="%s" and MWEG.module_exam_end_date>="%s") and MWEG.docstatus=1
+												"""%(student_list[0],self.module_exam_start_date,self.module_exam_end_date),as_dict=True)
+		else:
+			student_gr_data=frappe.db.sql(""" select MWEG.name,MWEG.exam_declaration_id,MWES.student_no,MWES.student_name, MWES.roll_no,MWES.group_name 
+												from `tabModule Wise Exam Group` MWEG
+												Join `tabModule Wise Exam Student` MWES ON MWEG.name=MWES.parent where MWES.student_no IN %s 
+												and (MWEG.module_exam_start_date <="%s" and MWEG.module_exam_end_date>="%s") and MWEG.docstatus=1
+												"""%(str(tuple(student_list)),self.module_exam_start_date,self.module_exam_end_date),as_dict=True)
+		
+		module_gr_name=[]
+		for t in student_gr_data:
+			module_gr_name.append(t['name'])
+		
+		module_gr_name=list(set(module_gr_name))
 
-	print(student_gr_data)
-	# a.s
-	pass
+		st_gp_exam_time_data=frappe.get_all("Student Group Exam Scheduling",{"parent":["IN",module_gr_name]},
+							['parent','group_name','examination_date','from_time','to_time','total_duration_in_hours'])
+
+		
+		for t in student_gr_data:
+			for j in st_gp_exam_time_data:
+				if t['name']==j['parent'] and t['group_name']==j['group_name']:
+					t['group_name']=j['group_name']
+					t['examination_date']=j['examination_date'] 
+					t['from_time']=j['from_time']
+					t['to_time']=j['to_time']
+					t['total_duration_in_hours']=j['total_duration_in_hours']
+		
+
+		comp_list_stu=[]
+		for t in self.get('student_list'):
+			dic={}
+			dic['student_no']=t.student_no
+			dic['group_name']=t.group_name
+			dic['name']=t.parent
+			for j in self.get('scheduling_group_exam'):
+				if t.group_name==j.group_name:
+					dic['group_name']=j.group_name
+					dic['examination_date']=datetime.strptime(j.examination_date , '%Y-%m-%d').date()
+					from_time=datetime.strptime(j.from_time, '%H:%M:%S').time()
+					from_time_time_delta = timedelta(
+						hours=from_time.hour,
+						minutes=from_time.minute,
+						seconds=from_time.second
+					)
+					dic['from_time']=from_time_time_delta
+					to_time=datetime.strptime(j.to_time, '%H:%M:%S').time()
+					to_time_time_delta = timedelta(
+						hours=to_time.hour,
+						minutes=to_time.minute,
+						seconds=to_time.second
+					)
+					dic['to_time']=to_time_time_delta
+					dic['total_duration_in_hours']=j.total_duration_in_hours
+			comp_list_stu.append(dic)
+
+		exam_over_lapping=[]
+		for t in student_gr_data:
+			for j in comp_list_stu:
+				if t['student_no']==j['student_no'] and t['examination_date']==j['examination_date'] and (t['from_time']<=j['from_time'] and t['to_time']>=j['to_time']):	
+					exam_over_lapping.append(t)
+
+
+		final_list=[]
+		for t in exam_over_lapping:
+			a={}
+			a['Exam Declaration Id']=t['exam_declaration_id']
+			a['Module Wise Exam Group Id']=t['name']
+			a['Student No']=t['student_no']
+			a['Student Name']=t['student_name']
+			a['roll_no']=t['roll_no']
+			a['Examination Date']="%s"%(t['examination_date'])
+			a['Group Name']=t['group_name']
+			a['From Time']='%s'%(t['from_time'])
+			a['To Time']='%s'%(t['to_time'])
+			a['Total Duration In Hours']='%s'%(t['total_duration_in_hours'])
+			final_list.append(a)
+
+
+		if final_list:
+			frappe.throw("<b>Overlapping Of Exam Schedule </b> <br> <i>%s</i>"%(final_list))
 
 
 
