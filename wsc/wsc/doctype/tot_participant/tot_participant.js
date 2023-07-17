4bd301d111ec1e6399455fe058d85134043ea422
toshiffshaik <tousiff.taj@soulunileaders.com>
2023-07-17 07:07:48 +0530

    fix tot bug

--- wsc/wsc/doctype/tot_participant/tot_participant.js
+++ wsc/wsc/doctype/tot_participant/tot_participant.js
@@ -2,30 +2,30 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('ToT Participant', {
-	onload:function(frm){
-			var dialog = new frappe.ui.Dialog({
-				fields: [
-					{
-						"label" : "Date",
-                        "fieldname": "course_schedule_date",
-                        "fieldtype": "Date",
-                        "reqd":1,
-					}
-				],
-				primary_action: function() {
-					var data = dialog.get_values();
-						// frm.set_query("course_schedule", function () {
-						// 	return {
-						// 		filters: {
-						// 			"schedule_date":data.course_schedule_date
-						// 		}
-						// 	}
-						// });
-					dialog.hide(); 
-				}
-			});
-			dialog.show();
-    },
+// 	onload:function(frm){
+// 			var dialog = new frappe.ui.Dialog({
+// 				fields: [
+// 					{
+// 						"label" : "Date",
+//                         "fieldname": "course_schedule_date",
+//                         "fieldtype": "Date",
+//                         "reqd":1,
+// 					}
+// 				],
+// 				primary_action: function() {
+// 					var data = dialog.get_values();
+// 						// frm.set_query("course_schedule", function () {
+// 						// 	return {
+// 						// 		filters: {
+// 						// 			"schedule_date":data.course_schedule_date
+// 						// 		}
+// 						// 	}
+// 						// });
+// 					dialog.hide(); 
+// 				}
+// 			});
+// 			dialog.show();
+//     },
 //    onload: function(frm) {
 //         frm.add_field({
 //             fieldname: 'your_fieldname',
