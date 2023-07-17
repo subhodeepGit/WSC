f91b7df6b0c6c229e9842d44c1bb13e86f840303
SNSoul414 <sukalyan.nayak@soulunileaders.com>
2023-07-17 15:57:46 +0530

    Commiting Employee Resignation

--- wsc/wsc/notification/custom_notification.py
+++ wsc/wsc/notification/custom_notification.py
@@ -923,4 +923,45 @@ def module_exam_group_data():
             # print(exam_declaration_id)
 
 
-    
+    #################  Notification Coding for Employee Resignation #######################
+
+# def sendHR(doc):
+#     sub="""<p><b>Employee Resignation</b></p><br>"""
+#     msg="""<b>---------------------Resignation Details---------------------</b><br>"""
+#     msg+="""<b>Resignation:</b>  {0}<br>""".format(doc['name'])
+#     msg+="""<b>Status:</b>  {0}<br>""".format(doc['current_status'])
+#     resignation_url = get_url_to_form('Employee Resignation', doc['name'])
+#     msg += """<b>Open Now:</b>  <a href="{0}">Click here</a><br>""".format(resignation_url)
+#     msg+="""<p><b>Initiate the Separation Process for the Employee if it is Approved</b></p><br>"""
+#     send_mail([doc['hr_mail']],sub,msg)
+#     frappe.msgprint("Confirmation mail sent to HR",[doc['hr_mail']])
+# def sendEmployee(doc):
+#     sub="""<p><b>Employee Resignation</b></p><br>"""
+#     msg="""<b>---------------------Resignation Details---------------------</b><br>"""
+#     msg+="""<b>Resignation:</b>  {0}<br>""".format(doc['name'])
+#     msg+="""<b>Status:</b>  {0}<br>""".format(doc['current_status'])
+#     resignation_url = get_url_to_form('Employee Resignation', doc['name'])
+#     msg += """<b>Open Now:</b>  <a href="{0}">Click here</a><br>""".format(resignation_url)
+#     send_mail([doc['employee_mail']],sub,msg)
+#     frappe.msgprint("Confirmation mail sent to Employee",[doc['employee_mail']])
+# def sendRa(doc):
+#     sub="""<p><b>Employee Resignation</b></p><br>"""
+#     msg="""<b>---------------------Resignation Details---------------------</b><br>"""
+#     msg+="""<b>Resignation:</b>  {0}<br>""".format(doc['name'])
+#     msg+="""<b>Status:</b>  {0}<br>""".format(doc['current_status'])
+#     resignation_url = get_url_to_form('Employee Resignation', doc['name'])
+#     msg += """<b>Open Now:</b>  <a href="{0}">Click here</a><br>""".format(resignation_url)
+#     send_mail([doc['ra_mail']],sub,msg)
+#     frappe.msgprint("Mail sent to Rpeorting Authority for Approval",[doc['ra_mail']])
+# def sendDh(doc):
+#     pass
+# def sendDirector(doc):
+#     sub="""<p><b>Employee Resignation</b></p><br>"""
+#     msg="""<b>---------------------Resignation Details---------------------</b><br>"""
+#     msg+="""<b>Resignation:</b>  {0}<br>""".format(doc['name'])
+#     msg+="""<b>Status:</b>  {0}<br>""".format(doc['current_status'])
+#     resignation_url = get_url_to_form('Employee Resignation', doc['name'])
+#     msg += """<b>Open Now:</b>  <a href="{0}">Click here</a><br>""".format(resignation_url)
+#     send_mail([doc['director_mail']],sub,msg)
+#     frappe.msgprint("Mail sent to Director for Approval",[doc['director_mail']])
+
