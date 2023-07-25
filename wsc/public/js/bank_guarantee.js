ca231b6cd4ef7870107cd95747570c51f02070b9
Subhajit-SOUL <subhajit.saha@soulunileaders.com>
2023-07-25 11:26:50 +0530

    removed comment from Bank Gurantee

--- wsc/public/js/bank_guarantee.js
+++ wsc/public/js/bank_guarantee.js
@@ -1,6 +1,5 @@
 frappe.ui.form.on('Bank Guarantee', {
     onload: function(frm) {
-        alert("HI")
         frm.set_query('reference_document', function() {
             return {
                 filters: {
