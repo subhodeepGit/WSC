e4245d4b8592ffb571dd99876d949cdaf052c406
abhishekadhikari88 <aadhikari@kiit.ac.in>
2023-07-26 13:06:44 +0530

    update in user permission dept.

--- wsc/wsc/doctype/user_permission_for_multiple_departments/user_permission_for_multiple_departments.js
+++ wsc/wsc/doctype/user_permission_for_multiple_departments/user_permission_for_multiple_departments.js
@@ -1,18 +1,14 @@
 // Copyright (c) 2022, SOUL Limited and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('User Permission for multiple Departments', {
-	// refresh: function(frm) {
-
-	// }
-	onload: function(frm) {
-	frm.set_query("department","departments", function(_doc, cdt, cdn) {
-		var d = locals[cdt][cdn];
-		return {
-			filters: {
-				'is_group': 0,
-			}
+frappe.ui.form.on('Division Child', {
+	departments_add: function(frm){
+		frm.fields_dict['departments'].grid.get_field('department').get_query = function(doc){
+			var department_list = [];
+			$.each(doc.departments, function(idx, val){
+				if (val.department) department_list.push(val.department);
+			});
+			return { filters: [['Department', 'name', 'not in', department_list],['is_group','=',0]] };
 		};
-	});
-}
+	}
 });
