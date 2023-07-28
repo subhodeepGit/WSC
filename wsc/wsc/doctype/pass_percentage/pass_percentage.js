2cec6ae2bc648a15010a84f9cd11865baf654664
toshiffshaik <tousiff.taj@soulunileaders.com>
2023-07-28 10:32:43 +0530

    pass percentage report

--- wsc/wsc/doctype/pass_percentage/pass_percentage.js
+++ wsc/wsc/doctype/pass_percentage/pass_percentage.js
@@ -10,5 +10,30 @@ frappe.ui.form.on('Pass Percentage', {
                 }
             };
         });
-	}
+	},
+    get__details(frm){
+        if (frm.doc.academic_year && frm.doc.academic_term && frm.doc.program_grade) {
+            frappe.call({
+                method: 'get_details',
+                doc:frm.doc,
+                callback: function(r) {
+                    if (r.message) {
+                        frm.set_value("remarks",r.message)
+                    }
+                    frm.refresh();
+                    refresh_field('programs_pass_');
+                }
+            });
+        }
+    },
+    academic_term: function(frm) {
+		frm.trigger("get__details");
+	},
+	academic_year: function(frm) {
+		frm.trigger("get__details");
+	},
+    program_grade: function(frm) {
+		frm.trigger("get__details");
+	},
 });
+
