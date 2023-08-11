9693c5bb2fc7e6901a49fcd0f848f8feb7356519
Rupalib-soul <rupali.bhatta@soulunileaders.com>
2023-08-11 12:55:21 +0530

    updated response time for redirection.

--- wsc/wsc/hdfcIntegration/ccavRequestHandler.py
+++ wsc/wsc/hdfcIntegration/ccavRequestHandler.py
@@ -54,7 +54,7 @@ def ccavResponseHandler():
 
 
 if __name__ == '__main__':
-	app.run(host = '127.0.0.1', port = 8080)
+	app.run(host = '127.0.0.1', debug = True, port = 8080)
 	# serve(app, host='0.0.0.0', port=5000, url_scheme='https')
 
 
