92631b5ecb15d3755fb6410b6c5e85e47a7dea5d
sayantanghosh22 <sayantan.ghosh@soulunileaders.com>
2023-08-18 11:03:15 +0530

    tot-final

---
+++ wsc/wsc/doctype/corporate_calendar/corporate_calendar_calendar.js
@@ -0,0 +1,11 @@
+frappe.views.calendar["Corporate Calendar"] = {
+	field_map: {
+		"start": "start_date",
+		"end": "end_date",
+		"id": "name",
+		"title": "event_name",
+        "start_date" : "start_date",
+        "end_date" : "end_date",
+	},
+	gantt: false,
+}
