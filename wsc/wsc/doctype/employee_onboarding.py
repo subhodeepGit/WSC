429c3d2d087f928b059db12af3001a355c3e630d
SarthakRath <sarthak.srath@soulunileaders.com>
2023-08-24 17:04:21 +0530

    commiting employee onboarding notification

---
+++ wsc/wsc/doctype/employee_onboarding.py
@@ -0,0 +1,36 @@
+import frappe
+from frappe import _
+from frappe.model.document import Document
+
+from wsc.wsc.notification.custom_notification import maildirector,mailhr
+def validate(doc,method):
+        
+        if doc.workflow_state == "Pending Approval from Director Admin":
+            director_mail(doc)
+        if doc.workflow_state == "Approved" or doc.workflow_state=="Rejected":
+            hr_mail(doc)
+
+def director_mail(doc):
+    director_mail = frappe.get_all("User",filters={'role':"Director"},pluck='name')
+    if director_mail:
+        director_mail_id = director_mail[0]
+        data={}
+        data["director_mail"]=director_mail_id
+        data["employee_onboarding"]=doc.name
+        data["current_status"]=doc.workflow_state
+        data["name"]=doc.name
+        maildirector(data)
+    else :
+        frappe.throw("Setup Director User ID")
+def hr_mail(doc):
+    hr_mail=frappe.get_all("User",filters={'role':"HR Admin"},pluck='name')
+    if hr_mail:
+        hr_mail_id = hr_mail[0]
+        data ={}
+        data["hr_mail"]=hr_mail_id
+        data["employee_onboarding"]=doc.name
+        data["current_status"]=doc.workflow_state
+        data["name"]=doc.name
+        mailhr(data)
+    else :
+        frappe.throw("Setup HR Admin User ID")
