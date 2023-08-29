b5a108d14b31b194f18365e67f827d898e2a23b4
SarthakRath <sarthak.srath@soulunileaders.com>
2023-08-29 12:30:38 +0530

    comiting data fetching code employee grievance

--- wsc/public/js/employee_grievance.js
+++ wsc/public/js/employee_grievance.js
@@ -8,5 +8,40 @@ frappe.ui.form.on('Employee Grievance', {
                 }
             };
         });
+
+        frm.set_query("investigating_authority", function() {
+            return {
+                query: "wsc.wsc.validations.employee_grievance.get_cell_members",
+                filters: {
+                    "investigation_cell": frm.doc.investigation_cell
+                }
+            };
+        });
+        frm.set_query("resolved_by", function() {
+            return {
+                query: "wsc.wsc.validations.employee_grievance.get_cell_members",
+                filters: {
+                    "investigation_cell": frm.doc.investigation_cell
+                }
+            };
+        });
+    },
+    investigating_authority:function(frm){
+		frappe.call({
+			method: 'wsc.wsc.validations.employee_grievance.get_cell_member_details',
+			args: {
+				"investigating_authority":frm.doc.investigating_authority
+			},
+			callback: function(r) {
+				if (r.message) {
+					if (r.message[0]['employee_name']){
+						frm.set_value("authority_name",r.message[0]['employee_name'])
+					}
+					if (r.message[0]['user_id']){
+						frm.set_value("authority_user_id",r.message[0]['user_id'])
+					}
+                }
+            }
+        })
     }
 });
