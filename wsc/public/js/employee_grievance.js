9bab718986e07c78edbd178206a02749aabce398
SarthakRath <sarthak.srath@soulunileaders.com>
2023-08-29 13:34:28 +0530

    commting Changes in employee grievance

--- wsc/public/js/employee_grievance.js
+++ wsc/public/js/employee_grievance.js
@@ -1,47 +1,47 @@
 frappe.ui.form.on('Employee Grievance', {
-    setup: function(frm) {
-        frm.set_query("investigation_cell", function() {
-            return {
-                query: "wsc.wsc.validations.employee_grievance.get_cell",
-                filters: {
-                    "grievance_type": frm.doc.grievance_type
-                }
-            };
-        });
+    // setup: function(frm) {
+    //     frm.set_query("investigation_cell", function() {
+    //         return {
+    //             query: "wsc.wsc.validations.employee_grievance.get_cell",
+    //             filters: {
+    //                 "grievance_type": frm.doc.grievance_type
+    //             }
+    //         };
+    //     });
 
-        frm.set_query("investigating_authority", function() {
-            return {
-                query: "wsc.wsc.validations.employee_grievance.get_cell_members",
-                filters: {
-                    "investigation_cell": frm.doc.investigation_cell
-                }
-            };
-        });
-        frm.set_query("resolved_by", function() {
-            return {
-                query: "wsc.wsc.validations.employee_grievance.get_cell_members",
-                filters: {
-                    "investigation_cell": frm.doc.investigation_cell
-                }
-            };
-        });
-    },
-    investigating_authority:function(frm){
-		frappe.call({
-			method: 'wsc.wsc.validations.employee_grievance.get_cell_member_details',
-			args: {
-				"investigating_authority":frm.doc.investigating_authority
-			},
-			callback: function(r) {
-				if (r.message) {
-					if (r.message[0]['employee_name']){
-						frm.set_value("authority_name",r.message[0]['employee_name'])
-					}
-					if (r.message[0]['user_id']){
-						frm.set_value("authority_user_id",r.message[0]['user_id'])
-					}
-                }
-            }
-        })
-    }
+    //     frm.set_query("investigating_authority", function() {
+    //         return {
+    //             query: "wsc.wsc.validations.employee_grievance.get_cell_members",
+    //             filters: {
+    //                 "investigation_cell": frm.doc.investigation_cell
+    //             }
+    //         };
+    //     });
+    //     frm.set_query("resolved_by", function() {
+    //         return {
+    //             query: "wsc.wsc.validations.employee_grievance.get_cell_members",
+    //             filters: {
+    //                 "investigation_cell": frm.doc.investigation_cell
+    //             }
+    //         };
+    //     });
+    // },
+    // investigating_authority:function(frm){
+	// 	frappe.call({
+	// 		method: 'wsc.wsc.validations.employee_grievance.get_cell_member_details',
+	// 		args: {
+	// 			"investigating_authority":frm.doc.investigating_authority
+	// 		},
+	// 		callback: function(r) {
+	// 			if (r.message) {
+	// 				if (r.message[0]['employee_name']){
+	// 					frm.set_value("authority_name",r.message[0]['employee_name'])
+	// 				}
+	// 				if (r.message[0]['user_id']){
+	// 					frm.set_value("authority_user_id",r.message[0]['user_id'])
+	// 				}
+    //             }
+    //         }
+    //     })
+    // }
 });
--- wsc/wsc/validations/employee_grievance.py
+++ wsc/wsc/validations/employee_grievance.py
@@ -9,35 +9,35 @@ def validate(self,method):
         employee_grievance_employee_mail(self)    
 
 
-@frappe.whitelist()
-def get_cell(doctype, txt, searchfield, start, page_len, filters):
-    investigation_cell = frappe.get_all(
-        "Employee Grievance Cell",
-        {"grievance_type":filters.get("grievance_type")},
-        ["name"],as_list=1
-    )
+# @frappe.whitelist()
+# def get_cell(doctype, txt, searchfield, start, page_len, filters):
+#     investigation_cell = frappe.get_all(
+#         "Employee Grievance Cell",
+#         {"grievance_type":filters.get("grievance_type")},
+#         ["name"],as_list=1
+#     )
 
-    member_names = [member for member in investigation_cell]
+#     member_names = [member for member in investigation_cell]
 
-    return member_names
+#     return member_names
 
-@frappe.whitelist()
-def get_cell_members(doctype, txt, searchfield, start, page_len, filters):
-    investigation_cell_members = frappe.get_all(
-        "Grievance Members",
-        {"parent":filters.get("investigation_cell")},
-        ["employee","employee_name"],as_list=1
-    )
-    return investigation_cell_members
+# @frappe.whitelist()
+# def get_cell_members(doctype, txt, searchfield, start, page_len, filters):
+#     investigation_cell_members = frappe.get_all(
+#         "Grievance Members",
+#         {"parent":filters.get("investigation_cell")},
+#         ["employee","employee_name"],as_list=1
+#     )
+#     return investigation_cell_members
 
-@frappe.whitelist()
-def get_cell_member_details(investigating_authority):
-    cell_member_details = frappe.get_all(
-        "Employee",
-        {"name":investigating_authority},
-        ["employee_name","user_id"]
-    )
-    print("\n\n\n\n\nCell Members Details")
-    print(cell_member_details)
-    return cell_member_details
+# @frappe.whitelist()
+# def get_cell_member_details(investigating_authority):
+#     cell_member_details = frappe.get_all(
+#         "Employee",
+#         {"name":investigating_authority},
+#         ["employee_name","user_id"]
+#     )
+#     print("\n\n\n\n\nCell Members Details")
+#     print(cell_member_details)
+#     return cell_member_details
 
