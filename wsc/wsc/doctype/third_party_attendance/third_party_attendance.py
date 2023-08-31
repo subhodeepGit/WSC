4ced2608120dd3667e2bf6db9cd854d56555066a
deysubhodeep <subhodeep.dey@soulunileaders.com>
2023-08-31 18:16:32 +0530

    validations added

--- wsc/wsc/doctype/third_party_attendance/third_party_attendance.py
+++ wsc/wsc/doctype/third_party_attendance/third_party_attendance.py
@@ -1,8 +1,28 @@
 # Copyright (c) 2023, SOUL Limited and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
+from frappe.utils import formatdate, get_link_to_form, getdate
 from frappe.model.document import Document
 
 class ThirdPartyAttendance(Document):
-	pass
+	def validate(self):
+		attendance_record = None
+		attendance_record = frappe.db.exists(
+			"Third Party Attendance",
+			{
+				"third_party_attendance_contract": self.third_party_attendance_contract,
+				"date": self.date,
+				"docstatus": ("!=", 2),
+				"name": ("!=", self.name),
+			},
+		)
+		if attendance_record:
+			record = get_link_to_form("Student Attendance", attendance_record)
+			frappe.throw(
+				_("Attendance record {0} already exists against the Date {1}").format(
+					record, frappe.bold(self.date)
+				),
+				title=_("Duplicate Entry"),
+			)
