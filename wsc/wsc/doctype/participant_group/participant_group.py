c01e5128aeec20f6e536981b8c0723adb8928983
abhishekadhikari88 <aadhikari@kiit.ac.in>
2023-09-06 21:22:30 +0530

    updata in paticipant group

--- wsc/wsc/doctype/participant_group/participant_group.py
+++ wsc/wsc/doctype/participant_group/participant_group.py
@@ -11,35 +11,35 @@ class ParticipantGroup(Document):
 	def validate(self):
 		dupicate_student_group_chk(self)
 		self.calculate_total_hours()
-		for d in self.get("classes"):
-			parent_doc = frappe.new_doc("ToT Class Schedule")
-			parent_doc.participant_group_id = self.name
-			parent_doc.academic_year = self.academic_year
-			parent_doc.academic_term = self.academic_term
-			parent_doc.course_name = self.program
-			parent_doc.module_id = self.course
-			parent_doc.module_name = self.module_name
-			parent_doc.scheduled_date = d.scheduled_date
-			# parent_doc.scheduled_time = d.scheduled_time
-			parent_doc.room_number = d.room_number
-			parent_doc.room_name = d.room_name
-			parent_doc.from_time = d.from_time
-			parent_doc.to_time = d.to_time
-			parent_doc.duration = d.duration
+		# for d in self.get("classes"):
+		# 	parent_doc = frappe.new_doc("ToT Class Schedule")
+		# 	parent_doc.participant_group_id = self.name
+		# 	parent_doc.academic_year = self.academic_year
+		# 	parent_doc.academic_term = self.academic_term
+		# 	parent_doc.course_name = self.program
+		# 	parent_doc.module_id = self.course
+		# 	parent_doc.module_name = self.module_name
+		# 	parent_doc.scheduled_date = d.scheduled_date
+		# 	# parent_doc.scheduled_time = d.scheduled_time
+		# 	parent_doc.room_number = d.room_number
+		# 	parent_doc.room_name = d.room_name
+		# 	parent_doc.from_time = d.from_time
+		# 	parent_doc.to_time = d.to_time
+		# 	parent_doc.duration = d.duration
 
-			for td1 in self.get('participants'):
-				parent_doc.append('participants', {
-					'participant_id' : td1.participant,
-					'participant_name' : td1.participant_name,
-					# 'group_roll_number' : td1.group_roll_number,
-					'active' : td1.active,	
-				})
-			for td2 in self.get('instructor'):
-				parent_doc.append('trainers', {
-					'trainer_id' : td2.instructors,
-					'trainer_name' : td2.instructor_name	
-				})
-			parent_doc.save()	
+		# 	for td1 in self.get('participants'):
+		# 		parent_doc.append('participants', {
+		# 			'participant_id' : td1.participant,
+		# 			'participant_name' : td1.participant_name,
+		# 			# 'group_roll_number' : td1.group_roll_number,
+		# 			'active' : td1.active,	
+		# 		})
+		# 	for td2 in self.get('instructor'):
+		# 		parent_doc.append('trainers', {
+		# 			'trainer_id' : td2.instructors,
+		# 			'trainer_name' : td2.instructor_name	
+		# 		})
+		# 	# parent_doc.save()	
 
 		dulicate_trainer_chk(self)
 		class_scheduling_date_validation(self)
