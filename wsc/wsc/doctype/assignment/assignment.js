d63fbaae1d90d1b79d02621efbd1e7f87904a039
sayantanghosh22 <sayantan.ghosh@soulunileaders.com>
2023-09-11 18:10:36 +0530

    link validation for tot

--- wsc/wsc/doctype/assignment/assignment.js
+++ wsc/wsc/doctype/assignment/assignment.js
@@ -8,7 +8,7 @@ frappe.ui.form.on('Assignment', {
 	setup: function(frm){
 		frm.set_query("instructor_id", function() {
 			return {
-				query: 'wsc.wsc.doctype.tot_participant_attendance.tot_participant_attendance.instructor',
+				query: 'wsc.wsc.doctype.assignment.assignment.instructor',
 				filters:{"participant_group_id":frm.doc.participant_group}
 				
 			};
@@ -16,8 +16,16 @@ frappe.ui.form.on('Assignment', {
 
 		frm.set_query("participant_id", function() {
 			return {
-				query: 'wsc.wsc.doctype.tot_participant_attendance.tot_participant_attendance.participant',
+				query: 'wsc.wsc.doctype.assignment.assignment.participant',
 				filters:{"participant_group_id":frm.doc.participant_group}
+				
+			};
+		});
+
+		frm.set_query("assessment_criteria", function() {
+			return {
+				query: 'wsc.wsc.doctype.assignment.assignment.criteria',
+				filters:{"course":frm.doc.course}
 				
 			};
 		});
@@ -33,8 +41,6 @@ frappe.ui.form.on('Assignment', {
 				frm.set_value("academic_term", result.message[1]) // acadmic term
 				frm.set_value("programs", result.message[2]) // course
 				frm.set_value("course", result.message[3]) // module
-				// frm.set_df_property('instructor_id', 'options', result.message[4]) // instructor_id
-				frm.set_df_property('assessment_criteria', 'options', result.message[5]) // assessment criteria
 			}
 		})
 	},
