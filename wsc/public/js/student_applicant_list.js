672df88efcb18cb5a8dcf0d5e075c4f8269e8078
toshiffshaik <tousiff.taj@soulunileaders.com>
2023-09-13 17:30:14 +0530

    changes made wrt student applicant form

--- wsc/public/js/student_applicant_list.js
+++ wsc/public/js/student_applicant_list.js
@@ -1,14 +1,29 @@
-frappe.listview_settings['Student Applicant'] = {
-    refresh: function(listview) {
-        if (!frappe.route_options & frappe.user.has_role(["Applicant"]) && !frappe.user.has_role(["System Manager"])){ 
-            $(".filter-selector").hide();
-               frappe.route_options = {
-                "owner": ["=", frappe.session.user],
-				// "docstatus":1
-            };
+// frappe.listview_settings['Student Applicant'] = {
+//     refresh: function(listview) {
+//         if (!frappe.route_options & frappe.user.has_role(["Applicant"]) && !frappe.user.has_role(["System Manager"])){ 
+//             alert("SYSTEM")
+//             // $(".filter-selector").hide();
+//                frappe.route_options = {
+//                 // "owner": ["=", frappe.session.user],
+//                 "student_email_id":["=", frappe.session.user]
+// 				// "docstatus":1
+//             };
      
+//         }
+//     }
+// };
+frappe.listview_settings['Student Applicant'] = {
+    onload: function(listview) {
+        if (frappe.user.has_role(["Applicant"]) && !frappe.user.has_role(["System Manager"])){        
+            if(frappe.route_options){
+                frappe.route_options = {
+                    "student_email_id": ["=", frappe.session.user]
+                };
+                $(".filter-selector").hide();
+                
+            }
         }
-    }
+    }      
 };
 //  if not self.owner== frappe.session.user:
       // frappe.route_options = {
