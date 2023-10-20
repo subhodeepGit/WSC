690f8c45419299dc8da2124003da19ac56cfa314
abhishekadhikari88 <aadhikari@kiit.ac.in>
2023-10-20 15:14:47 +0600

    correcting identified bugs in T&P module

--- wsc/wsc/doctype/tnp_event/tnp_event.js
+++ wsc/wsc/doctype/tnp_event/tnp_event.js
@@ -3,7 +3,26 @@
 
 frappe.ui.form.on('TnP Event', {
 	refresh: function(frm) {
-
+		if(frm.is_new()){
+			frm.set_df_property('event_status', 'options', ['Scheduled'])
+		}
+		else{
+			frm.set_df_property('event_status', 'options', ['Scheduled', 'Completed', 'Cancelled'])
+		}
+		frm.set_query('select_program', function(){
+			return{
+				filters:{
+					'program_status' : 'Scheduled',
+				}
+			}
+		})
+	},
+	setup: function(frm){
+		const date = new Date()
+		let year = date.getFullYear()
+		let month = String(date.getMonth() + 1).padStart(2,'0')
+		let day = String(date.getDate()).padStart(2,'0')
+		frm.set_value('current_date', `${year}-${month}-${day}`)
 	},
 	in_a_program : function(frm){
 		if(frm.doc.in_a_program == 0){
@@ -11,7 +30,6 @@ frappe.ui.form.on('TnP Event', {
 			frm.set_value('program_name', '')
 		}
 	},
-
 	program_start_date: function(frm){
 		if(frm.doc.event_start_date && frm.doc.event_start_date < frm.doc.program_start_date){
 			frappe.throw("Event start date cannot be before program start date")
@@ -37,6 +55,9 @@ frappe.ui.form.on('TnP Event', {
 		if(frm.doc.event_start_date && frm.doc.event_start_date > frm.doc.event_end_date){
 			frappe.throw("Event start date cannot be after event end date")
 		}
+		if(frm.doc.event_start_date < frm.doc.current_date){
+			frappe.throw('Start date cannot be before current date')
+		}
 	},
 	event_end_date: function(frm){
 		if(frm.doc.event_end_date && frm.doc.program_end_date && frm.doc.program_start_date){
@@ -47,95 +68,10 @@ frappe.ui.form.on('TnP Event', {
 		if(frm.doc.event_end_date < frm.doc.event_start_date){
 			frappe.throw("Event end date cannot be before event start date")
 		}
+		if(frm.doc.event_end_date < frm.doc.current_date){
+			frappe.throw('End date cannot be before current date')
+		}
 	},
-
-
-	// program_start_date: function(frm){
-	// 	if(frm.doc.event_start_date){
-	// 		if(frm.doc.event_start_date < frm.doc.program_start_date || frm.doc.event_start_date > frm.doc.program_end_date){
-			
-	// 		}
-	// 	}
-	// 	if(frm.doc.event_end_date){
-	// 		if(frm.doc.event_start_date < frm.doc.program_start_date || frm.doc.event_start_date > frm.doc.program_end_date){
-			
-	// 		}
-	// 	}
-	// },
-	// program_end_date: function(frm){
-	// 	if(frm.doc.event_end_date){
-	// 		if(frm.doc.event_start_date < frm.doc.program_start_date || frm.doc.event_start_date > frm.doc.program_end_date){
-			
-	// 		}
-	// 	}
-	// 	if(frm.doc.event_start_date){
-	// 		if(frm.doc.event_start_date < frm.doc.program_start_date || frm.doc.event_start_date > frm.doc.program_end_date){
-			
-	// 		}
-	// 	}
-	// },
-	// event_start_date: function(frm){
-	// 	if(in_a_program == 1 && (frm.doc.event_start_date < frm.doc.program_start_date || frm.doc.event_start_date > frm.doc.program_end_date)){
-	// 		frappe.throw("Event start date cannot be outside of program dates")
-	// 	}		
-	// 	if(frm.doc.event_start_date > frm.doc.event_end_date){
-	// 		frappe.throw("Event Start Date should be less than Event End date");
-	// 	}
-	// },
-	// event_end_date: function(frm){
-	// 	if(in_a_program == 1 && (frm.doc.event_end_date < frm.doc.program_start_date || frm.doc.event_end_date > frm.doc.program_end_date)){
-	// 		frappe.throw("Event start date cannot be outside of program dates")
-	// 	}		
-	// 	if(frm.doc.event_end_date < frm.doc.event_start_date){
-	// 		frappe.throw("Event End Date should be greater than Event Start date");
-	// 	}
-	// },
-	// // -------------------------------------------------------------------
-
-
-	// event_start_date : function(frm){
-	// 	if(frm.doc.event_start_date < frm.doc.program_start_date || frm.doc.event_start_date > frm.doc.program_end_date){
-	// 		frappe.throw("Event Start Date cannot be outside of the program dates");
-	// 	}
-	// 	if(frm.doc.event_start_date && frm.doc.event_end_date){
-	// 		if(frm.doc.event_start_date > frm.doc.event_end_date){
-	// 			frappe.throw("Event Start Date should be Less than Event End date");
-	// 		}
-	// 	}
-	// 	if(frm.doc.event_date && frm.doc.event_start_date){
-	// 		if(frm.doc.event_date != frm.doc.event_start_date){
-	// 			frappe.throw("Event Date and Event Start Date must be same");
-	// 		}
-	// 	}
-	// },
-	// event_end_date:function(frm){
-	// 	if(frm.doc.event_end_date < frm.doc.program_start_date || frm.doc.event_end_date > frm.doc.program_end_date){
-	// 		frappe.throw("Event Start Date cannot be outside of the program dates");
-	// 	}
-	// 	if(frm.doc.event_start_date && frm.doc.event_end_date){
-	// 		if(frm.doc.event_end_date < frm.doc.event_start_date){
-	// 			frappe.throw("Event End Date should be Greater than Event Start date");
-	// 		}
-	// 	}
-	// },
-	// event_date : function(frm){
-	// 	frm.set_value('event_start_date', frm.doc.event_date)
-	// 	if(frm.doc.event_date && frm.doc.event_start_date){ // checking if both the event_date and event_start date has values
-	// 		if(frm.doc.event_date >= frm.doc.program_start_date && frm.doc.event_date <= frm.doc.program_end_date){ // checking if the event date is within the program dates range
-	// 			if(frm.doc.event_date != frm.doc.event_start_date){ // checking if event date and event start date has same value
-	// 				frappe.throw("Event Date and Event Start Date must be same");
-	// 			}
-	// 		}
-	// 	}
-	// },
-	// event_date : function(frm){
-	// 	frm.set_value('event_start_date', frm.doc.event_date)
-	// 	if(frm.doc.event_date && frm.doc.event_start_date){
-	// 		if(frm.doc.event_date != frm.doc.event_start_date){
-	// 			frappe.throw("Event Date and Event Start Date must be same");
-	// 		}
-	// 	}
-	// },
 	start_time : function(frm){
 		if(frm.doc.start_time && frm.doc.end_time){
 			if(frm.doc.start_time > frm.doc.end_time){
