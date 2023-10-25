ffdddb2b9151688f8dd6754c43c6d8fa0ea0e178
sayantanghosh22 <sayantan.ghosh@soulunileaders.com>
2023-10-25 11:06:10 +0600

    selection round report

--- wsc/wsc/report/selection_round_report/selection_round_report.py
+++ wsc/wsc/report/selection_round_report/selection_round_report.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2023, SOUL Limited and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from dataclasses import field
 import frappe
 from frappe import _
@@ -9,29 +9,56 @@ import itertools
 from datetime import datetime
 
 def execute(filters=None):
-    pe_data , course_schedule_data=get_data(filters)
-    get_columns_info=get_columns()
-    return  get_columns_info,pe_data
+    data = get_data(filters)
+    columns = get_columns(filters)
+    return columns,data
 
 
 
 def get_data(filters=None):
-    pass
+    student_id=filters.get('student_id')
 
-def get_columns():
+    filter=[]
+    if student_id:
+        filter.append(["student_no", "=", student_id])
+
+    data = frappe.get_all("Selection Round", filters=filter, fields=['name','student_no', 'student_name', 'company_name', 'round_of_placement'])
+    
+    return data
+
+
+def get_columns(filters=None):
     columns = [
         {
             "label": _("Student No"),
-            "fieldname": "student",
+            "fieldname": "student_no",
             "fieldtype": "Link",
             "options": "Student",
             "width":180
         },
         {
-            "label": _("Student Name"),
-            "fieldname": "student_name",
+            "label": _("Name"),
+            "fieldname": "name",
             "fieldtype": "Data",
             "width":160
         },
+        {
+            "label": _("Student Name"),
+            "fieldname": "student_name",
+            "fieldtype": "Data",
+            "width":160 
+        },
+         {
+            "label": _("Company Name"),
+            "fieldname": "company_name",
+            "fieldtype": "Data",
+            "width":160 
+        },
+         {
+            "label": _("Placement Round"),
+            "fieldname": "round_of_placement",
+            "fieldtype": "Data",
+            "width":160 
+        }
     ]
     return columns
