572d3873656c0f5300c0bb2260e17abcc249c5e2
SPriytesh <priytesh.shah@soulunileaders.com>
2023-11-02 16:05:52 +0530

    Bug fixes for Item screen

--- wsc/wsc/doctype/item.py
+++ wsc/wsc/doctype/item.py
@@ -4,7 +4,26 @@ def validate(doc, method):
     validate_rates(doc)
 
 def validate_rates(doc):
-    if doc.valuation_rate < 0:
-        frappe.throw("<B>Valuation rate</B> cannot be less than negative")
-    if doc.standard_rate < 0:
-        frappe.throw("<B>Standard rate</B> cannot be less than negative")
+    if doc.valuation_rate:
+        if doc.valuation_rate < 0:
+            frappe.throw("<B>Valuation rate</B> cannot be negative")
+    if doc.standard_rate:
+        if doc.standard_rate < 0:
+            frappe.throw("<B>Standard rate</B> cannot be negative")
+    if doc.shelf_life_in_days:
+        if doc.shelf_life_in_days < 0:
+            frappe.throw("<B>Shelf Life</B> cannot be negative")
+
+    if doc.warranty_period:
+        int_warranty_period = int(doc.warranty_period)
+        if int_warranty_period <= 0:
+            frappe.throw("<B>Warranty Period</B> cannot be negative or Zero")
+    if doc.weight_per_unit: 
+        if doc.weight_per_unit < 0:
+            frappe.throw("<B>Weight Per Unit</B> cannot be negative")
+    if doc.min_order_qty: 
+        if doc.min_order_qty < 0:
+            frappe.throw("<B>Minimum order quantity</B> cannot be negative")
+    if doc.safety_stock: 
+        if doc.safety_stock < 0:
+            frappe.throw("<B>Safety Stock</B> cannot be negative")
--- wsc/wsc/validations/item_price.py
+++ wsc/wsc/validations/item_price.py
@@ -6,3 +6,6 @@ from wsc.wsc.notification.custom_notification import changed_impaneled_price
 def validate(self,method):
 	if self.workflow_state == "Approved by CFO":
 		changed_impaneled_price(self)
+	
+	if self.supllier_email is None:
+		frappe.throw("Enter <b>Supplier email id</b> in <b>Supplier</b> Form")
