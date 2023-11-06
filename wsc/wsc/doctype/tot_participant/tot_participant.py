f71f22dc5413799b9095d685f4ed38d5e39fb60a
deysubhodeep <subhodeep.dey@soulunileaders.com>
2023-11-06 18:37:42 +0530

    Bug Fix: 1. Aadhar number field : Field must be taking integer input only and Length of Aadhar number field should be 16 digits only 2.Pincode field:  It should accept Digits only and The length should be 6 digits

--- wsc/wsc/doctype/tot_participant/tot_participant.py
+++ wsc/wsc/doctype/tot_participant/tot_participant.py
@@ -6,6 +6,8 @@ from frappe.model.document import Document
 
 class ToTParticipant(Document):
     def validate(self):
+        aadhar_number_validation(self)
+        pin_code_validation(self)
         phone_no_vlaidation(self)    
         self.participant_name = " ".join(
         filter(None, [self.first_name, self.middle_name, self.last_name])
@@ -24,4 +26,25 @@ def phone_no_vlaidation(self):
                 if self.name==t['name']:
                     flag="Yes"
             if flag=="No":        
-                frappe.throw("Participant Mobile Number should be Unique")   
+                frappe.throw("Participant Mobile Number should be Unique")
+
+
+def aadhar_number_validation(self):
+
+    if self.adhaar_number:
+        if not (self.adhaar_number).isdigit():
+            frappe.throw("Field <b>Adhaar Number</b> Accept Digits Only")
+        if len(self.adhaar_number)>16:
+            frappe.throw("Field <b>Adhaar Number</b> must be 16 Digits")
+        if len(self.adhaar_number)<16:
+            frappe.throw("Field <b>Adhaar Number</b> must be 16 Digits")   
+
+def pin_code_validation(self):
+
+    if self.pincode:
+        if not (self.pincode).isdigit():
+            frappe.throw("Field <b>Pincode</b> Accept Digits Only")
+        if len(self.pincode)>6:
+            frappe.throw("Field <b>Pincode</b> must be 6 Digits")
+        if len(self.pincode)<6:
+            frappe.throw("Field <b>Pincode</b> must be 6 Digits")  
