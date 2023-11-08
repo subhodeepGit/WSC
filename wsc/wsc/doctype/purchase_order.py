054c7f00af7910b9185c0f224acc326e687559f3
SPriytesh <priytesh.shah@soulunileaders.com>
2023-11-08 16:33:24 +0530

    Procurment & Inventory management bug fixes

--- wsc/wsc/doctype/purchase_order.py
+++ wsc/wsc/doctype/purchase_order.py
@@ -8,53 +8,66 @@ def validate(self,method):
 		all_items_received(self)
 
 	if self.additional_discount_percentage:
-		if self.additional_discount_percentage <0:
-			frappe.throw("Total Discount Percentage cannnot be negative")
+		if self.additional_discount_percentage:
+			if self.additional_discount_percentage <0:
+				frappe.throw("Total Discount Percentage cannnot be negative")
 
 	if self.discount_amount:
-		if self.discount_amount <0:
-			frappe.throw("Discount Amount cannnot be negative")
+		if self.discount_amount:
+			if self.discount_amount <0:
+				frappe.throw("Discount Amount cannnot be negative")
 
 	for cd in self.items:
-		if cd.price_list_rate < 0:
-			frappe.throw("Price List Rate for Item cannot be negative")
+		if cd.price_list_rate:
+			if cd.price_list_rate < 0:
+				frappe.throw("Price List Rate for Item cannot be negative")
 
 	for cd in self.items:
-		if cd.discount_percentage < 0:
-			frappe.throw("Discount Percentage for Item cannot be negative")
+		if cd.discount_percentage:
+			if cd.discount_percentage < 0:
+				frappe.throw("Discount Percentage for Item cannot be negative")
 	
 	for cd in self.items:
-		if cd.discount_amount < 0:
-			frappe.throw("Discount Amount for Item cannot be negative")
+		if cd.discount_amount:
+			if cd.discount_amount < 0:
+				frappe.throw("Discount Amount for Item cannot be negative")
 
 	for cd in self.items:
-		if cd.rate < 0:
-			frappe.throw("Rate for Item cannot be negative")
+		if cd.rate:
+			if cd.rate < 0:
+				frappe.throw("Rate for Item cannot be negative")
 
 	for cd in self.items:
-		if cd.margin_rate_or_amount < 0:
-			frappe.throw("Margin Rate for Item cannot be negative")
+		if cd.margin_rate_or_amount:
+			if cd.margin_rate_or_amount < 0:
+				frappe.throw("Margin Rate for Item cannot be negative")
 
 	for cd in self.items:
-		if cd.rate_with_margin < 0:
-			frappe.throw("Rate with margin for Item cannot be negative")
+		if cd.rate_with_margin:
+			if cd.rate_with_margin < 0:
+				frappe.throw("Rate with margin for Item cannot be negative")
 
 	for cd in self.taxes:
-		if cd.rate < 0:
-			frappe.throw("Tax Rate cannot be negative")
+		if cd.rate:
+			if cd.rate < 0:
+				frappe.throw("Tax Rate cannot be negative")
 
 	for cd in self.payment_schedule:
-		if cd.invoice_portion < 0:
-			frappe.throw("Invoice Portion cannot be negative")
+		if cd.invoice_portion:
+			if cd.invoice_portion < 0:
+				frappe.throw("Invoice Portion cannot be negative")
 
 	for cd in self.payment_schedule:
-		if cd.discount < 0:
-			frappe.throw("Discount cannot be negative")
+		if cd.discount:
+			if cd.discount < 0:
+				frappe.throw("Discount cannot be negative")
 
 	for cd in self.payment_schedule:
-		if cd.payment_amount < 0:
-			frappe.throw("Payment Amount cannot be negative")
+		if cd.payment_amount:
+			if cd.payment_amount < 0:
+				frappe.throw("Payment Amount cannot be negative")
 
 	for cd in self.payment_schedule:
-		if cd.base_payment_amount < 0:
-			frappe.throw("Payment Amount cannot be negative")
+		if cd.base_payment_amount:
+			if cd.base_payment_amount < 0:
+				frappe.throw("Payment Amount cannot be negative")
--- wsc/wsc/validations/purchase_invoice.py
+++ wsc/wsc/validations/purchase_invoice.py
@@ -2,45 +2,56 @@ import frappe
 
 def validate(self, method):
 	for cd in self.items:
-		if cd.price_list_rate < 0:
-			frappe.throw("Price List Rate for Item cannot be negative")
+		if cd.price_list_rate:
+			if cd.price_list_rate < 0:
+				frappe.throw("Price List Rate for Item cannot be negative")
 			
             
 	for cd in self.items:
-		if cd.discount_percentage < 0:
-			frappe.throw("Discount on Price List Rate (%) for Item cannot be negative")
+		if cd.discount_percentage:
+			if cd.discount_percentage < 0:
+				frappe.throw("Discount on Price List Rate (%) for Item cannot be negative")
 			
 	for cd in self.items:
-		if cd.discount_amount < 0:
-			frappe.throw("Discount Amount for Item cannot be negative")
+		if cd.discount_amount:
+			if cd.discount_amount < 0:
+				frappe.throw("Discount Amount for Item cannot be negative")
 			
 	for cd in self.items:
-		if cd.rate < 0:
-			frappe.throw("Rate for Item cannot be negative")
+		if cd.rate:
+			if cd.rate < 0:
+				frappe.throw("Rate for Item cannot be negative")
 			
 	for cd in self.items:
-		if cd.margin_rate_or_amount < 0:
-			frappe.throw("Margin Rate or Amount for Item cannot be negative")
+		if cd.margin_rate_or_amount:
+			if cd.margin_rate_or_amount < 0:
+				frappe.throw("Margin Rate or Amount for Item cannot be negative")
 	if self.write_off_amount:
 		if self.write_off_amount < 0:
 			frappe.throw("Write off Amount cannot be negative")
 	
 	for cd in self.taxes:
-		if cd.rate < 0:
-			frappe.throw("Tax Rate cannot be negative")
+		if cd.rate:
+			if cd.rate < 0:
+				frappe.throw("Tax Rate cannot be negative")
 			
 	for cd in self.advances:
-		if cd.allocated_amount < 0:
-			frappe.throw("Allocated amount cannot be negative")
+		if cd.allocated_amount:
+			if cd.allocated_amount < 0:
+				frappe.throw("Allocated amount cannot be negative")
 	for cd in self.payment_schedule:
-		if cd.invoice_portion < 0:
-			frappe.throw("Invoice Portion cannot be negative")
+		if cd.invoice_portion:
+			if cd.invoice_portion < 0:
+				frappe.throw("Invoice Portion cannot be negative")
 	for cd in self.payment_schedule:
-		if cd.discount < 0:
-			frappe.throw("Discount cannot be negative")
+		if cd.discount:
+			if cd.discount < 0:
+				frappe.throw("Discount cannot be negative")
 	for cd in self.payment_schedule:
-		if cd.payment_amount < 0:
-			frappe.throw("Payment Amount cannot be negative")
+		if cd.payment_amount:
+			if cd.payment_amount < 0:
+				frappe.throw("Payment Amount cannot be negative")
 	for cd in self.payment_schedule:
-		if cd.paid_amount < 0:
-			frappe.throw("Paid Amount cannot be negative")
+		if cd.paid_amount:
+			if cd.paid_amount < 0:
+				frappe.throw("Paid Amount cannot be negative")
