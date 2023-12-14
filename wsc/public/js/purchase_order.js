9ef1d988a915200cf960cc1c24998468583aaa6f
SPriytesh <priytesh.shah@soulunileaders.com>
2023-12-14 18:56:30 +0530

    Removed Custom button

--- wsc/public/js/purchase_order.js
+++ wsc/public/js/purchase_order.js
@@ -7,6 +7,8 @@
 frappe.ui.form.on('Purchase Order', {                          
     onload_post_render: function(frm){                      
         frm.page.remove_inner_button('Product Bundle','Get Items From')
+        frm.page.remove_inner_button('Purchase Requisition','Get Items From')
+        frm.page.remove_inner_button('Supplier Quotation','Get Items From')
         frm.page.remove_inner_button('Update Rate as per Last Purchase', 'Tools')
         frm.page.remove_inner_button('Link to Material Request', 'Tools')
         frm.page.remove_inner_button('Hold', 'Status')
@@ -14,6 +16,7 @@ frappe.ui.form.on('Purchase Order', {
         frm.page.remove_inner_button('Subscription', 'Create')
 		frm.page.remove_inner_button('Payment Request', 'Create')
 		frm.page.remove_inner_button('Update Items')
+        frm.disable_('Get Items From');
         }
     }
 );
