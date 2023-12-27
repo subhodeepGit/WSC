1a16387ae57e65f4bedb9a6d31709c94242c1c7e
toshiffshaik <tousiff.taj@soulunileaders.com>
2023-12-27 09:52:44 +0530

    topic add course button hidden

--- wsc/public/js/topic.js
+++ wsc/public/js/topic.js
@@ -1,8 +1,6 @@
 frappe.ui.form.on('Topic',{
     refresh: function(frm) {
-        if (frappe.user.has_role(["Student","Instructor"]) && !frappe.user.has_role(["System Manager"]) ){
             frm.remove_custom_button("Add to Courses","Action");
-        }
     }
         
 })
