c9b6b605f4d74a19a82c9968f9efaf9a4edb3b61
AbhishekAdhikari2023 <abhishek.adhikari@soulunileaders.com>
2024-01-02 11:36:00 +0600

    bug fix in slcm

--- wsc/wsc/validations/grading_scale.py
+++ wsc/wsc/validations/grading_scale.py
@@ -3,4 +3,4 @@ import frappe
 def validate(doc,method):
     for t in doc.get("intervals"):
         if t.threshold <0:
-            frappe.throw("Threshold value can't Negative")
+            frappe.throw("Threshold value can't be Negative")
