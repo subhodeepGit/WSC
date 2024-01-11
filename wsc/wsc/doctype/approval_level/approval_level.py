f1f407042b860ad1d70915b95980097b62c0a960
AbhishekAdhikari2023 <abhishek.adhikari@soulunileaders.com>
2024-01-11 18:16:52 +0600

    update in dy workflow

--- wsc/wsc/doctype/approval_level/approval_level.py
+++ wsc/wsc/doctype/approval_level/approval_level.py
@@ -6,17 +6,26 @@ from frappe.model.document import Document
 
 class ApprovalLevel(Document):
 	def validate(self):
-		doc=frappe.new_doc("Role")
-		doc.role_name=self.name
-		doc.search_bar=0
-		doc.notifications=0
-		doc.list_sidebar=0
-		doc.bulk_actions=0
-		doc.view_switcher=0
-		doc.form_sidebar=0
-		doc.save()
+		role_data=frappe.get_all("Role",{"name":self.name})
+		if not role_data:
+			doc=frappe.new_doc("Role")
+			doc.role_name=self.name
+			doc.search_bar=0
+			doc.notifications=0
+			doc.list_sidebar=0
+			doc.bulk_actions=0
+			doc.view_switcher=0
+			doc.form_sidebar=0
+			doc.timeline=0
+			doc.dashboard=0
+			doc.save()
+		else:
+			doc=frappe.get_doc("Role",role_data[0]['name'])
+			doc.disabled=0
+			doc.save()
 
 	def on_trash(self):
 		doc=frappe.get_doc("Role",self.name)
-		doc.delete()
+		doc.disabled=1
+		doc.save()
 
