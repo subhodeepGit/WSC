aacfe04aa55c783f124223e4d67a1bc9e75464d5
SPriytesh <priytesh.shah@soulunileaders.com>
2024-01-29 14:27:50 +0530

    Updated options

--- wsc/public/js/contract.js
+++ wsc/public/js/contract.js
@@ -3,3 +3,9 @@ frappe.ui.form.on('Contract', {
 		frm.set_df_property('party_type', 'options', ['Supplier', 'Employee']);
 	}
 });	
+
+frappe.ui.form.on('Contract', {
+	refresh: function(frm) {
+		frm.set_df_property('document_type', 'options', ['Quotation', 'Project','Purchase Order','Purchase Invoice']);
+	}
+});	
