303dc501bead5603cdbaf5b71661bce933f0f979
SPriytesh <priytesh.shah@soulunileaders.com>
2024-01-30 11:24:24 +0530

    Role permission for Data import

--- wsc/wsc/validations/item_group.py
+++ wsc/wsc/validations/item_group.py
@@ -16,8 +16,13 @@ def validate_tax_amount(doc):
         if cd.minimum_net_rate and cd.maximum_net_rate:
             if cd.minimum_net_rate >= cd.maximum_net_rate:
                 frappe.throw("Minimum net rate <b>{0}</b> should be not greater than Maximum net rate <b>{1}</b>.".format(cd.minimum_net_rate, cd.maximum_net_rate))
+
 def validate_amount(doc):
     for cd in doc.authorization_table:
-           if cd.from_amount and cd.to_amount:
-            if cd.from_amount >= cd.to_amount:
-                frappe.throw("To amount <b>{0}</b> should not be greater than From amount <b>{1}</b>.".format(cd.to_amount, cd.from_amount))
+        if cd.to_amount < 0:
+            frappe.throw("To amount cannot be zero")
+        if cd.to_amount == 0:
+            if cd.from_amount>0:
+                frappe.throw("To amount cannot be zero")
+        if cd.from_amount > cd.to_amount:
+            frappe.throw("To amount <b>{0}</b> should be greater than From amount <b>{1}</b>.".format(cd.to_amount, cd.from_amount))
