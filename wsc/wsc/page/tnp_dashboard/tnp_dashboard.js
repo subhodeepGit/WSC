adcf7d94c89aed75acbd6fe719ae8e8b16c7cbb1
sayantanghosh22 <sayantan.ghosh@soulunileaders.com>
2024-02-05 16:16:06 +0600

    tnp dashboard

--- wsc/wsc/page/tnp_dashboard/tnp_dashboard.js
+++ wsc/wsc/page/tnp_dashboard/tnp_dashboard.js
@@ -4,4 +4,11 @@ frappe.pages['tnp-dashboard'].on_page_load = function(wrapper) {
 		title: 'Training and Placement Dashboard',
 		single_column: true
 	});
+
+	const container_list = document.querySelectorAll('.container')
+	container_list[1].remove()
+	
+	$(frappe.render_template("tnp_dashboard" , {
+		font_awesome_link:"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css",
+	})).appendTo(page.body)
 }
